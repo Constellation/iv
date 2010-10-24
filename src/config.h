f5c394cd6c9e0ddd10385972bf68858853343501
Constellation <utatane.tea@gmail.com>
2010-10-25 03:32:32 +0900

    delete config.h

--- src/config.h
+++
@@ -1,8 +0,0 @@
-#ifndef _IV_CONFIG_H
-#define _IV_CONFIG_H_
-
-// #define LITTLE_ENDIAN 1
-#define IV_VERSION "0.0.1"
-#define IV_DEVELOPER "Yusuke Suzuki <utatane.tea@gmail.com>"
-
-#endif  // _IV_CONFIG_H_
