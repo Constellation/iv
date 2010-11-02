ed57f8012f097d31aeccd0cb8d9067745f9d5c9f
Constellation <utatane.tea@gmail.com>
2010-11-02 22:48:24 +0900

    using type_traits about declareing AstVisitor / ConstAstVisitor

--- src/ast-visitor.h
+++ src/ast-visitor.h
@@ -1,8 +1,27 @@
 #ifndef _IV_AST_VISITOR_H_
 #define _IV_AST_VISITOR_H_
+#include <tr1/type_traits>
+#include "noncopyable.h"
 
 namespace iv {
 namespace core {
+namespace detail {
+
+template<bool IsConst, typename T>
+struct AstVisitorTraits {
+};
+
+template<typename T>
+struct AstVisitorTraits<true, T> {
+  typedef typename std::tr1::add_pointer<typename std::tr1::add_const<T>::type>::type type;
+};
+
+template<typename T>
+struct AstVisitorTraits<false, T> {
+  typedef typename std::tr1::add_pointer<typename std::tr1::add_const<T>::type>::type type;
+};
+
+}
 class AstNode;
 class Statement;
 class Block;
@@ -53,54 +72,60 @@ class IndexAccess;
 class FunctionCall;
 class ConstructorCall;
 
-class AstVisitor {
+template<bool IsConst>
+class BasicAstVisitor : private Noncopyable<BasicAstVisitor<IsConst> >::type {
+ private:
+  template<typename T>
+  struct add {
+    typedef typename detail::AstVisitorTraits<IsConst, T>::type type;
+  };
  public:
-  virtual ~AstVisitor() = 0;
-  virtual void Visit(Block* block) = 0;
-  virtual void Visit(FunctionStatement* func) = 0;
-  virtual void Visit(VariableStatement* var) = 0;
-  virtual void Visit(Declaration* decl) = 0;
-  virtual void Visit(EmptyStatement* empty) = 0;
-  virtual void Visit(IfStatement* ifstmt) = 0;
-  virtual void Visit(DoWhileStatement* dowhile) = 0;
-  virtual void Visit(WhileStatement* whilestmt) = 0;
-  virtual void Visit(ForStatement* forstmt) = 0;
-  virtual void Visit(ForInStatement* forstmt) = 0;
-  virtual void Visit(ContinueStatement* continuestmt) = 0;
-  virtual void Visit(BreakStatement* breakstmt) = 0;
-  virtual void Visit(ReturnStatement* returnstmt) = 0;
-  virtual void Visit(WithStatement* withstmt) = 0;
-  virtual void Visit(LabelledStatement* labelledstmt) = 0;
-  virtual void Visit(CaseClause* clause) = 0;
-  virtual void Visit(SwitchStatement* switchstmt) = 0;
-  virtual void Visit(ThrowStatement* throwstmt) = 0;
-  virtual void Visit(TryStatement* trystmt) = 0;
-  virtual void Visit(DebuggerStatement* debuggerstmt) = 0;
-  virtual void Visit(ExpressionStatement* exprstmt) = 0;
+  virtual ~BasicAstVisitor() = 0;
+  virtual void Visit(typename add<Block>::type block) = 0;
+  virtual void Visit(typename add<FunctionStatement>::type func) = 0;
+  virtual void Visit(typename add<VariableStatement>::type var) = 0;
+  virtual void Visit(typename add<Declaration>::type decl) = 0;
+  virtual void Visit(typename add<EmptyStatement>::type empty) = 0;
+  virtual void Visit(typename add<IfStatement>::type ifstmt) = 0;
+  virtual void Visit(typename add<DoWhileStatement>::type dowhile) = 0;
+  virtual void Visit(typename add<WhileStatement>::type whilestmt) = 0;
+  virtual void Visit(typename add<ForStatement>::type forstmt) = 0;
+  virtual void Visit(typename add<ForInStatement>::type forstmt) = 0;
+  virtual void Visit(typename add<ContinueStatement>::type continuestmt) = 0;
+  virtual void Visit(typename add<BreakStatement>::type breakstmt) = 0;
+  virtual void Visit(typename add<ReturnStatement>::type returnstmt) = 0;
+  virtual void Visit(typename add<WithStatement>::type withstmt) = 0;
+  virtual void Visit(typename add<LabelledStatement>::type labelledstmt) = 0;
+  virtual void Visit(typename add<CaseClause>::type clause) = 0;
+  virtual void Visit(typename add<SwitchStatement>::type switchstmt) = 0;
+  virtual void Visit(typename add<ThrowStatement>::type throwstmt) = 0;
+  virtual void Visit(typename add<TryStatement>::type trystmt) = 0;
+  virtual void Visit(typename add<DebuggerStatement>::type debuggerstmt) = 0;
+  virtual void Visit(typename add<ExpressionStatement>::type exprstmt) = 0;
 
-  virtual void Visit(Assignment* assign) = 0;
-  virtual void Visit(BinaryOperation* binary) = 0;
-  virtual void Visit(ConditionalExpression* cond) = 0;
-  virtual void Visit(UnaryOperation* unary) = 0;
-  virtual void Visit(PostfixExpression* postfix) = 0;
+  virtual void Visit(typename add<Assignment>::type assign) = 0;
+  virtual void Visit(typename add<BinaryOperation>::type binary) = 0;
+  virtual void Visit(typename add<ConditionalExpression>::type cond) = 0;
+  virtual void Visit(typename add<UnaryOperation>::type unary) = 0;
+  virtual void Visit(typename add<PostfixExpression>::type postfix) = 0;
 
-  virtual void Visit(StringLiteral* literal) = 0;
-  virtual void Visit(NumberLiteral* literal) = 0;
-  virtual void Visit(Identifier* literal) = 0;
-  virtual void Visit(ThisLiteral* literal) = 0;
-  virtual void Visit(NullLiteral* literal) = 0;
-  virtual void Visit(TrueLiteral* literal) = 0;
-  virtual void Visit(FalseLiteral* literal) = 0;
-  virtual void Visit(Undefined* literal) = 0;
-  virtual void Visit(RegExpLiteral* literal) = 0;
-  virtual void Visit(ArrayLiteral* literal) = 0;
-  virtual void Visit(ObjectLiteral* literal) = 0;
-  virtual void Visit(FunctionLiteral* literal) = 0;
+  virtual void Visit(typename add<StringLiteral>::type literal) = 0;
+  virtual void Visit(typename add<NumberLiteral>::type literal) = 0;
+  virtual void Visit(typename add<Identifier>::type literal) = 0;
+  virtual void Visit(typename add<ThisLiteral>::type literal) = 0;
+  virtual void Visit(typename add<NullLiteral>::type literal) = 0;
+  virtual void Visit(typename add<TrueLiteral>::type literal) = 0;
+  virtual void Visit(typename add<FalseLiteral>::type literal) = 0;
+  virtual void Visit(typename add<Undefined>::type literal) = 0;
+  virtual void Visit(typename add<RegExpLiteral>::type literal) = 0;
+  virtual void Visit(typename add<ArrayLiteral>::type literal) = 0;
+  virtual void Visit(typename add<ObjectLiteral>::type literal) = 0;
+  virtual void Visit(typename add<FunctionLiteral>::type literal) = 0;
 
-  virtual void Visit(IdentifierAccess* prop) = 0;
-  virtual void Visit(IndexAccess* prop) = 0;
-  virtual void Visit(FunctionCall* call) = 0;
-  virtual void Visit(ConstructorCall* call) = 0;
+  virtual void Visit(typename add<IdentifierAccess>::type prop) = 0;
+  virtual void Visit(typename add<IndexAccess>::type prop) = 0;
+  virtual void Visit(typename add<FunctionCall>::type call) = 0;
+  virtual void Visit(typename add<ConstructorCall>::type call) = 0;
  protected:
   template<typename T>
   class Acceptor {
@@ -127,83 +152,11 @@ class AstVisitor {
   };
 };
 
-inline AstVisitor::~AstVisitor() { }
+template<bool IsConst>
+inline BasicAstVisitor<IsConst>::~BasicAstVisitor() { }
 
-class ConstAstVisitor {
- public:
-  virtual ~ConstAstVisitor() = 0;
-  virtual void Visit(const Block* block) = 0;
-  virtual void Visit(const FunctionStatement* func) = 0;
-  virtual void Visit(const VariableStatement* var) = 0;
-  virtual void Visit(const Declaration* decl) = 0;
-  virtual void Visit(const EmptyStatement* empty) = 0;
-  virtual void Visit(const IfStatement* ifstmt) = 0;
-  virtual void Visit(const DoWhileStatement* dowhile) = 0;
-  virtual void Visit(const WhileStatement* whilestmt) = 0;
-  virtual void Visit(const ForStatement* forstmt) = 0;
-  virtual void Visit(const ForInStatement* forstmt) = 0;
-  virtual void Visit(const ContinueStatement* continuestmt) = 0;
-  virtual void Visit(const BreakStatement* breakstmt) = 0;
-  virtual void Visit(const ReturnStatement* returnstmt) = 0;
-  virtual void Visit(const WithStatement* withstmt) = 0;
-  virtual void Visit(const LabelledStatement* labelledstmt) = 0;
-  virtual void Visit(const CaseClause* clause) = 0;
-  virtual void Visit(const SwitchStatement* switchstmt) = 0;
-  virtual void Visit(const ThrowStatement* throwstmt) = 0;
-  virtual void Visit(const TryStatement* trystmt) = 0;
-  virtual void Visit(const DebuggerStatement* debuggerstmt) = 0;
-  virtual void Visit(const ExpressionStatement* exprstmt) = 0;
-
-  virtual void Visit(const Assignment* assign) = 0;
-  virtual void Visit(const BinaryOperation* binary) = 0;
-  virtual void Visit(const ConditionalExpression* cond) = 0;
-  virtual void Visit(const UnaryOperation* unary) = 0;
-  virtual void Visit(const PostfixExpression* postfix) = 0;
-
-  virtual void Visit(const StringLiteral* literal) = 0;
-  virtual void Visit(const NumberLiteral* literal) = 0;
-  virtual void Visit(const Identifier* literal) = 0;
-  virtual void Visit(const ThisLiteral* literal) = 0;
-  virtual void Visit(const NullLiteral* literal) = 0;
-  virtual void Visit(const TrueLiteral* literal) = 0;
-  virtual void Visit(const FalseLiteral* literal) = 0;
-  virtual void Visit(const Undefined* literal) = 0;
-  virtual void Visit(const RegExpLiteral* literal) = 0;
-  virtual void Visit(const ArrayLiteral* literal) = 0;
-  virtual void Visit(const ObjectLiteral* literal) = 0;
-  virtual void Visit(const FunctionLiteral* literal) = 0;
-
-  virtual void Visit(const IdentifierAccess* prop) = 0;
-  virtual void Visit(const IndexAccess* prop) = 0;
-  virtual void Visit(const FunctionCall* call) = 0;
-  virtual void Visit(const ConstructorCall* call) = 0;
- protected:
-  template<typename T>
-  class Acceptor {
-   public:
-    explicit Acceptor(T* visitor) :  visitor_(visitor) { }
-    template<typename U>
-    void operator()(U* p) const {
-      p->Accept(visitor_);
-    }
-   private:
-    T* const visitor_;
-  };
-
-  template<typename T>
-  class Visitor {
-   public:
-    explicit Visitor(T* visitor) :  visitor_(visitor) { }
-    template<typename U>
-    void operator()(U* p) const {
-      visitor_->Visit(p);
-    }
-   private:
-    T* const visitor_;
-  };
-};
-
-inline ConstAstVisitor::~ConstAstVisitor() { }
+typedef BasicAstVisitor<false> AstVisitor;
+typedef BasicAstVisitor<true> ConstAstVisitor;
 
 } }  // namespace iv::core
 #endif  // _IV_AST_VISITOR_H_
