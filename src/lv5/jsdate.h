d6f7b852d432d59e21042981564e36a05759d2f0
Constellation <utatane.tea@gmail.com>
2011-02-05 15:27:42 +0900

    rm jsdate.cc

--- src/lv5/jsdate.h
+++ src/lv5/jsdate.h
@@ -1,6 +1,7 @@
 #ifndef _IV_LV5_JSDATE_H_
 #define _IV_LV5_JSDATE_H_
 #include "jsobject.h"
+#include "context_utils.h"
 
 namespace iv {
 namespace lv5 {
@@ -9,24 +10,42 @@ class Context;
 
 class JSDate : public JSObject {
  public:
-  JSDate(Context* ctx, double val);
+  JSDate(Context* ctx, double val)
+    : value_(val) {
+  }
+
 
   JSVal DefaultValue(Context* ctx,
-                     Hint::Object hint, Error* res);
+                     Hint::Object hint, Error* res) {
+    return JSObject::DefaultValue(
+        ctx,
+        (hint == Hint::NONE) ? Hint::STRING : hint, res);
+  }
 
-  static JSDate* New(Context* ctx, double val);
+
+  static JSDate* New(Context* ctx, double val) {
+    JSDate* const date = new JSDate(ctx, val);
+    const Class& cls = context::Cls(ctx, "Date");
+    date->set_class_name(cls.name);
+    date->set_prototype(cls.prototype);
+    return date;
+  }
+
 
   static JSDate* NewPlain(Context* ctx, double val) {
     return new JSDate(ctx, val);
   }
 
+
   const double& value() const {
     return value_;
   }
 
+
   void set_value(double val) {
     value_ = val;
   }
+
 
  private:
   double value_;
