b6d21e4376867bc9063e25d0fbae859a681cdefa
Constellation <utatane.tea@gmail.com>
2011-02-21 01:44:55 +0900

    implement Date.parse

--- src/lv5/date_parser.h
+++ src/lv5/date_parser.h
@@ -1,6 +1,8 @@
 #ifndef _IV_LV5_DATE_PARSER_H_
 #define _IV_LV5_DATE_PARSER_H_
 #include <tr1/array>
+#include <algorithm>
+#include <limits>
 #include "noncopyable.h"
 #include "stringpiece.h"
 #include "lv5/date_utils.h"
@@ -67,6 +69,8 @@ static const std::tr1::array<const Keyword, 27> kKeywords = { {
   { "",    Keyword::NONE,  0  },
 } };
 
+static const int kNone = std::numeric_limits<int>::max();
+
 class KeywordChecker : private core::Noncopyable<KeywordChecker>::type {
  public:
 
@@ -88,7 +92,7 @@ class KeywordChecker : private core::Noncopyable<KeywordChecker>::type {
     }
    private:
     const buf_type& buf_;
-    std::size_t len_;
+    const std::size_t len_;
   };
 
   template<typename Iter>
@@ -115,36 +119,20 @@ class DateParser : private core::Noncopyable<DateParser>::type {
   class DateComponent : private core::Noncopyable<DateComponent>::type {
    public:
     DateComponent()
-      : year_(0),
-        month_(0),
-        date_(0) {
+      : month_(kNone),
+        slots_(),
+        pos_(0) {
+      std::fill(slots_.begin(), slots_.end(), kNone);
     }
-    void SetYear(int year) {
-      if (year < 100) {
-        if (year < 50) {
-          year_ = year + 2000;
-        } else {
-          year_ = year + 1900;
-        }
-      } else {
-        year_ = year;
-      }
-    }
+
     void SetMonth(int month) {
       month_ = month;
     }
-    void SetDate(int date) {
-      date_ = date;
-    }
 
-    bool IsValid() const {
-      if (!IsMonthExpecting(month_)) {
-        return false;
+    void Add(int n) {
+      if (pos_ < slots_.size()) {
+        slots_[pos_++] = n;
       }
-      if (!IsDateExpecting(date_)) {
-        return false;
-      }
-      return true;
     }
 
     static bool IsMonthExpecting(uint32_t month) {
@@ -156,12 +144,47 @@ class DateParser : private core::Noncopyable<DateParser>::type {
     }
 
     double MakeDay() const {
-      return date::MakeDay(year_, month_, date_);
+      if (month_ != kNone) {
+        // named month is explicitly defined
+        // DY
+        const int day = slots_[0];
+        const int year = CalcYear(slots_[1]);
+        if (day != kNone && year != kNone) {
+          return date::MakeDay(year, month_ - 1, day);
+        } else {
+          // invalid
+          return JSValData::kNaN;
+        }
+      } else {
+        // YMD
+        const int year = CalcYear(slots_[0]);
+        const int month = slots_[1];
+        const int day = slots_[2];
+        if (year != kNone && day != kNone && year != kNone) {
+          return date::MakeDay(year, month - 1, day);
+        } else {
+          return JSValData::kNaN;
+        }
+      }
     }
+
    private:
-    int year_;
+
+    static int CalcYear(int year) {
+      if (year < 100) {
+        if (year < 50) {
+          return year + 2000;
+        } else {
+          return year + 1900;
+        }
+      } else {
+        return year;
+      }
+    }
+
     int month_;
-    int date_;
+    std::tr1::array<int, 3> slots_;
+    std::size_t pos_;
   };
 
   class TimeComponent : private core::Noncopyable<TimeComponent>::type {
@@ -170,43 +193,59 @@ class DateParser : private core::Noncopyable<DateParser>::type {
       : hour_(0),
         min_(0),
         sec_(0),
-        msec_(0) {
+        msec_(0),
+        offset_(kNone) {
     }
+
     void SetHour(int hour) {
       hour_ = hour;
     }
+
     void SetMin(int min) {
       min_ = min;
     }
+
     void SetSec(int sec) {
       sec_ = sec;
     }
+
     void SetMSec(int msec) {
       msec_ = msec;
     }
 
-    bool IsValid() const {
-      if (!IsHourExpecting(hour_)) {
-        return false;
-      }
-      if (!IsMinExpecting(min_)) {
-        return false;
-      }
-      if (!IsSecExpecting(sec_)) {
-        return false;
-      }
-      if (!IsMSecExpecting(msec_)) {
-        return false;
-      }
-      return true;
+    void SetOffset(int offset) {
+      offset_ = offset;
     }
 
     double MakeTime() const {
-      return date::MakeTime(hour_, min_, sec_, msec_);
+      if (!IsHourExpecting(hour_)) {
+        return JSValData::kNaN;
+      }
+      if (!IsMinExpecting(min_)) {
+        return JSValData::kNaN;
+      }
+      if (!IsSecExpecting(sec_)) {
+        return JSValData::kNaN;
+      }
+      if (!IsMSecExpecting(msec_)) {
+        return JSValData::kNaN;
+      }
+      int hour = hour_;
+      if (offset_ != kNone) {
+        if (!IsOffsetHourExpecting(hour_)) {
+          return JSValData::kNaN;
+        }
+        hour = hour_ % 12 + offset_;
+      }
+      return date::MakeTime(hour, min_, sec_, msec_);
     }
 
     static bool IsHourExpecting(uint32_t hour) {
       return hour < 24;
+    }
+
+    static bool IsOffsetHourExpecting(uint32_t hour) {
+      return hour <= 12;
     }
 
     static bool IsMinExpecting(uint32_t min) {
@@ -225,22 +264,50 @@ class DateParser : private core::Noncopyable<DateParser>::type {
     int min_;
     int sec_;
     int msec_;
+    int offset_;
   };
 
   class TimezoneComponent : private core::Noncopyable<TimezoneComponent>::type {
    public:
     TimezoneComponent()
-      : adjust_(0) {
-    }
-    void SetAdjust(int adjust) {
-      adjust_ = adjust;
+      : hour_(0),
+        min_(0),
+        sign_(1) {
     }
 
-    bool IsValid() const {
-      return true;
+    bool IsUTC() const {
+      return hour_ == 0 && min_ == 0;
+    }
+
+    void SetSign(int sign) {
+      sign_ = sign;
+    }
+
+    void SetHour(int hour) {
+      hour_ = hour;
+    }
+
+    void SetMin(int min) {
+      min_ = min;
+    }
+
+    void SetTimezone(int tz) {
+      if (tz < 0) {
+        sign_ = -1;
+        hour_ = -tz;
+      } else {
+        hour_ = tz;
+      }
+      min_ = 0;
+    }
+
+    double MakeTz(double value) const {
+      return value - (sign_ * (hour_ * date::kMsPerHour + min_ * date::kMsPerMinute));
     }
    private:
-    int adjust_;
+    int hour_;
+    int min_;
+    int sign_;
   };
 
  public:
@@ -284,24 +351,20 @@ double DateParser::Parse(const String& str) {
       if (it != last) {
         if (*it == ':') {
           // HH pattern
-          std::cout << "hour:" << value << std::endl;
           time_.SetHour(value);
           ++it;
           if (it != last && core::character::IsDigit(*it)) {
             const uint32_t min = ReadUInt32(&it, last);
-            std::cout << "min:" << min << std::endl;
             time_.SetMin(min);
             if (*it == ':') {
               ++it;
               if (it != last && core::character::IsDigit(*it)) {
                 const uint32_t sec = ReadUInt32(&it, last);
-                std::cout << "sec:" << sec << std::endl;
                 time_.SetSec(sec);
                 if (*it == '.') {
                   ++it;
                   if (it != last && core::character::IsDigit(*it)) {
                     const uint32_t msec = ReadUInt32(&it, last);
-                    std::cout << "msec:" << msec << std::endl;
                     time_.SetMSec(msec);
                   }
                 }
@@ -310,48 +373,57 @@ double DateParser::Parse(const String& str) {
           }
         } else if (*it == '-') {
           // YYYY pattern
-          date_.SetYear(value);
-          std::cout << "year:" << value << std::endl;
+          date_.Add(value);
           ++it;
           if (it != last && core::character::IsDigit(*it)) {
             const uint32_t month = ReadUInt32(&it, last);
-            std::cout << "month:" << month << std::endl;
-            date_.SetMonth(month);
+            date_.Add(month);
             if (*it == '-') {
               ++it;
               if (it != last && core::character::IsDigit(*it)) {
                 const uint32_t d = ReadUInt32(&it, last);
-                std::cout << "day:" << d << std::endl;
-                date_.SetDate(d);
+                date_.Add(d);
               }
             }
           }
         } else {
-          std::cout << "year:" << value << std::endl;
-          date_.SetYear(value);
+          date_.Add(value);
         }
       } else {
-        std::cout << "year:" << value << std::endl;
-        date_.SetYear(value);
+        date_.Add(value);
       }
     } else if (core::character::IsASCIIAlpha(*it)) {
-      std::cout << "ALPHA" << std::endl;
       const Keyword keyword = KeywordChecker::Lookup(&it, last);
       if (keyword.IsNone()) {
-        std::cout << "NOT" << std::endl;
       } else if (keyword.IsMonth()) {
-        std::cout << "MONTH" << std::endl;
+        date_.SetMonth(keyword.value);
       } else if (keyword.IsAMPM()) {
-        std::cout << "AMPM" << std::endl;
+        time_.SetOffset(keyword.value);
       } else if (keyword.IsTimezone()) {
-        std::cout << "TZ" << std::endl;
+        tz_.SetTimezone(keyword.value);
       }
     } else if (*it == '(') {
       // skip paren
       ++it;
       for (; it != last; ++it) {
         if (*it == ')') {
+          ++it;
           break;
+        }
+      }
+    } else if ((*it == '+' || *it == '-') && tz_.IsUTC()) {
+      // sign
+      tz_.SetSign(*it == '+' ? 1 : -1);
+      ++it;
+      if (it != last && core::character::IsDigit(*it)) {
+        const uint32_t value = ReadUInt32(&it, last);
+        if (it != last && *it == ':') {
+          ++it;
+          tz_.SetHour(value);
+          tz_.SetMin(0);
+        } else {
+          tz_.SetHour(value / 100);
+          tz_.SetMin(value % 100);
         }
       }
     } else {
@@ -359,18 +431,14 @@ double DateParser::Parse(const String& str) {
     }
   }
 
-  if (date_.IsValid() && time_.IsValid() && tz_.IsValid()) {
-    return date::UTC(date::MakeDate(date_.MakeDay(),
-                                    time_.MakeTime()));
-  }
-  return JSValData::kNaN;
+  return tz_.MakeTz(date::MakeDate(date_.MakeDay(),
+                                   time_.MakeTime()));
 }
 
 template<typename String>
 double Parse(const String& str) {
-  return 0.0;
-//  DateParser parser;
-//  return parser.Parse(str);
+  DateParser parser;
+  return parser.Parse(str);
 }
 
 } } }  // namespace iv::lv5::date
