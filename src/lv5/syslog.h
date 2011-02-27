3b4264d2b7617cf71db38cf7d87b85280aced815
Constellation <utatane.tea@gmail.com>
2011-02-28 02:30:45 +0900

    syslog

---
+++ src/lv5/syslog.h
@@ -0,0 +1,28 @@
+// this syslog header file only used in debug
+#ifndef _IV_LV5_SYSLOG_H_
+#define _IV_LV5_SYSLOG_H_
+#include <cstdio>
+#include <string>
+#include <syslog.h>
+#include "noncopyable.h"
+namespace iv {
+namespace lv5 {
+
+class Syslog : private core::Noncopyable<Syslog>::type {
+ public:
+  explicit Syslog(const std::string& ident) {
+    openlog(ident.c_str(), LOG_CONS, LOG_USER);
+  }
+
+  ~Syslog() {
+    closelog();
+  }
+
+  template<int priority>
+  void Log(const std::string& message) {
+    syslog(priority, "%s", message.c_str());
+  }
+};
+
+} }  // namespace iv::lv5
+#endif  // _IV_LV5_SYSLOG_H_
