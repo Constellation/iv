46790a366d3617860614ba194f8105695fcfe029
Constellation <utatane.tea@gmail.com>
2011-04-08 00:48:02 +0900

    add railgun_fwd.h

---
+++ src/lv5/railgun_fwd.h
@@ -0,0 +1,17 @@
+#ifndef _IV_LV5_RAILGUN_FWD_H_
+#define _IV_LV5_RAILGUN_FWD_H_
+namespace iv {
+namespace lv5 {
+namespace railgun {
+
+class VM;
+class CodeContext;
+class Code;
+
+class Compiler;
+
+template<typename Derived>
+class DisAssembler;
+
+} } }  // namespace iv::lv5::railgun
+#endif  // _IV_LV5_RAILGUN_FWD_H_
