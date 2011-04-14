c52d22e6af2cf8df1da13ed86638c285cfc4b9e4
Constellation <utatane.tea@gmail.com>
2011-04-14 18:27:54 +0900

    add UTF16CodeToUTF8

--- src/unicode.h
+++ src/unicode.h
@@ -5,6 +5,30 @@
 #include <tr1/array>
 namespace iv {
 namespace core {
+namespace detail {
+
+static const std::tr1::array<int, 6> kUTF8Table1 = { {
+  0x7f, 0x7ff, 0xffff, 0x1fffff, 0x3ffffff, 0x7fffffff
+} };
+
+static const std::tr1::array<int, 6> kUTF8Table2 = { {
+  0,    0xc0, 0xe0, 0xf0, 0xf8, 0xfc
+} };
+
+static const std::tr1::array<int, 6> kUTF8Table3 = { {
+  0xff, 0x1f, 0x0f, 0x07, 0x03, 0x01
+} };
+
+// length table
+static const std::tr1::array<uint8_t, 0x40> kUTF8Table4 = { {
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+  2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+  3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5
+} };
+
+}  // namespace iv::core::detail
+
 
 inline int UCS4ToUTF8(uint32_t uc, uint8_t* buf) {
   assert(uc <= 0x10FFFF);
@@ -50,6 +74,30 @@ inline uint32_t UTF8ToUCS4(const uint8_t* buf, uint32_t size) {
     }
     return uc;
   }
+}
+
+// This function takes an integer value in the range 0 - 0x7fffffff
+// and encodes it as a UTF-8 character in 0 to 6 bytes.
+//
+// Arguments:
+// cvalue     the character value
+// buffer     pointer to buffer for result - at least 6 bytes long
+//
+// Returns:     number of characters placed in the buffer
+inline int UTF16CodeToUTF8(int cvalue, uint8_t* buffer) {
+  int i = 0;
+  for (const int len = detail::kUTF8Table1.size(); i < len; i++) {
+    if (cvalue <= detail::kUTF8Table1[i]) {
+      break;
+    }
+  }
+  buffer += i;
+  for (int j = i; j > 0; j--) {
+    *buffer-- = 0x80 | (cvalue & 0x3f);
+    cvalue >>= 6;
+  }
+  *buffer = detail::kUTF8Table2[i] | cvalue;
+  return i + 1;
 }
 
 } }  // namespace iv::core
