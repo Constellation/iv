630bc50c4e38f9c8a606a6436cd29f4d26b48252
Constellation <utatane.tea@gmail.com>
2011-04-19 15:13:27 +0900

    add BSD & Cygwin macro in platform.h

--- src/platform.h
+++ src/platform.h
@@ -1,6 +1,10 @@
 #ifndef _IV_PLATFORM_H_
 #define _IV_PLATFORM_H_
 
+#if (defined(__unix__) || defined(unix)) && !defined(USG)
+#include <sys/param.h>
+#endif
+
 #if defined(WIN32) || defined(WIN64)
 #ifndef OS_WIN
 #define OS_WIN 1
@@ -13,6 +17,14 @@
 #ifndef OS_LINUX
 #define OS_LINUX 1
 #endif  // OS_LINUX
+#elif defined(BSD)
+#ifndef OS_BSD
+#define OS_BSD 1
+#endif  // OS_BSD
+#elif defined(__CYGWIN__)
+#ifndef OS_CYGWIN
+#define OS_CYGWIN 1
+#endif  // OS_CYGWIN
 #endif
 
 #endif  // _IV_PLATFORM_H_
