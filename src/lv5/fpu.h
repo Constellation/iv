a63959cf1d6362f24b190d1e38a3469072ede130
Constellation <utatane.tea@gmail.com>
2011-05-08 01:49:43 +0900

    fix FPU

--- src/lv5/fpu.h
+++ src/lv5/fpu.h
@@ -1,7 +1,6 @@
 #ifndef _IV_LV5_FPU_H_
 #define _IV_LV5_FPU_H_
 #include "noncopyable.h"
-
 #if (defined(__GNUC__) && defined(__i386__) && !defined(__CYGWIN__))
 #include <fpu_control.h>
 namespace iv {
@@ -11,16 +10,21 @@ namespace lv5 {
 // so fix to double
 // see http://code.google.com/p/v8/issues/detail?id=144
 //     https://bugzilla.mozilla.org/show_bug.cgi?id=264912
+// 0x127f ?
 class FPU : private core::Noncopyable<> {
  public:
-  FPU() {
-    fpu_control_t cw = 0x127f;
+  FPU() : prev_() {
+    _FPU_GETCW(prev_);
+    fpu_control_t cw = (prev_ & ~0x300) | _FPU_DOUBLE;
     _FPU_SETCW(cw);
   }
+
   ~FPU() {
-    fpu_control_t cw = _FPU_DEFAULT;
-    _FPU_SETCW(cw);
+    _FPU_SETCW(prev_);
   }
+
+ private:
+  fpu_control_t prev_;
 };
 
 } }  // namespace iv::lv5
@@ -33,5 +37,4 @@ class FPU : private core::Noncopyable<> {
 
 } }  // namespace iv::lv5
 #endif
-
 #endif  // _IV_LV5_FPU_H_
