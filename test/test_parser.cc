d73086937d4507c7e99a3b8e4fc48d078f2e866c
Constellation <utatane.tea@gmail.com>
2011-05-09 23:45:08 +0900

    add parser test

--- test/test_parser.cc
+++ test/test_parser.cc
@@ -2,6 +2,7 @@
 #include <string>
 #include "space.h"
 #include "ast_factory.h"
+#include "ast_serializer.h"
 #include "parser.h"
 
 class AstFactory
@@ -9,7 +10,12 @@ class AstFactory
     public iv::core::ast::BasicAstFactory<AstFactory> { };
 
 TEST(ParserCase, StringTest) {
+  typedef iv::core::Parser<AstFactory, std::string> Parser;
   AstFactory factory;
   std::string src("var i = 20;");
-  iv::core::Parser<AstFactory, std::string> parser(&factory, src);
+  Parser parser(&factory, src);
+  const Parser::FunctionLiteral* global = parser.ParseProgram();
+  EXPECT_TRUE(global);
+  iv::core::ast::AstSerializer<AstFactory> serializer;
+  global->Accept(&serializer);
 }
