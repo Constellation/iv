6a35ea7e6e76beb0b1994f7d53b83ed9381c876a
Constellation <utatane.tea@gmail.com>
2011-05-13 14:06:41 +0900

    fix FixedStringBuilder

--- src/fixed_string_builder.h
+++ src/fixed_string_builder.h
@@ -5,44 +5,47 @@
 #include <cassert>
 #include <cstddef>
 #include <cstring>
+#include <string>
 #include <algorithm>
 #include "stringpiece.h"
 #include "fixed_container.h"
 namespace iv {
 namespace core {
 
-class FixedStringBuilder {
+template<class CharT>
+class BasicFixedStringBuilder {
  public:
-  FixedStringBuilder(char* ptr, std::size_t size)
+  BasicFixedStringBuilder(CharT* ptr, std::size_t size)
     : buffer_(ptr, size),
-      point_(0) { }
+      point_(0) {
+    // fill in destructor, at least, size should be more than 0.
+    assert(size != 0);
+  }
 
-  ~FixedStringBuilder() {
+  ~BasicFixedStringBuilder() {
     if (!IsFinalized()) {
       Finalize();
     }
   }
 
-  void AddCharacter(char ch) {
+  void AddCharacter(CharT ch) {
     assert(ch != '\0');
-    assert(!IsFinalized() && static_cast<std::size_t>(point_) < buffer_.size());
+    assert(!IsFinalized() && (point_ + 1) < buffer_.size());
     buffer_[point_++] = ch;
   }
 
-  void AddString(const StringPiece& piece) {
+  void AddString(const BasicStringPiece<CharT>& piece) {
     AddSubstring(piece.data(), piece.size());
   }
 
-  void AddSubstring(const char* s, int n) {
-    assert(!IsFinalized() &&
-           static_cast<std::size_t>(point_ + n) < buffer_.size());
-    assert(static_cast<std::size_t>(n) <= std::strlen(s));
+  void AddSubstring(const CharT* s, std::size_t n) {
+    assert(!IsFinalized() && (point_ + n) < buffer_.size());
     std::copy(s, s + n, buffer_.begin() + point_);
     point_ += n;
   }
 
   void AddInteger(int n) {
-    assert(IsFinalized() && static_cast<std::size_t>(point_) < buffer_.size());
+    assert(!IsFinalized() && point_ < buffer_.size());
     int ndigits = 0;
     int n2 = n;
     do {
@@ -50,32 +53,37 @@ class FixedStringBuilder {
       n2 /= 10;
     } while (n2);
     point_ += ndigits;
-    int i = point_;
+    std::size_t i = point_;
+    assert(!IsFinalized() && (point_ + ndigits) < buffer_.size());
     do {
       buffer_[--i] = '0' + (n % 10);
       n /= 10;
     } while (n);
   }
 
-  void AddPading(char c, std::size_t count) {
-    for (std::size_t i = 0; i < count; ++i) {
-      AddCharacter(c);
-    }
+  void AddPading(CharT c, std::size_t count) {
+    assert(!IsFinalized() && (point_ + count) < buffer_.size());
+    std::fill_n(buffer_.begin() + point_, count, c);
+    point_ += count;
   }
 
-  char* Finalize() {
+  CharT* Finalize() {
+    assert(!IsFinalized() && (point_ + 1) < buffer_.size());
     buffer_[point_] = '\0';
+    point_ = std::string::npos;
     return buffer_.data();
   }
 
  private:
   bool IsFinalized() const {
-    return point_ < 0;
+    return point_ == std::string::npos;
   }
 
-  FixedContainer<char> buffer_;
-  int point_;
+  FixedContainer<CharT> buffer_;
+  std::size_t point_;
 };
+
+typedef BasicFixedStringBuilder<char> FixedStringBuilder;
 
 } }  // namespace iv::core
 #endif  // _IV_FIXED_STRING_BUILDER_H_
--- test/test_fixed_string_builder.cc
+++ test/test_fixed_string_builder.cc
@@ -4,12 +4,34 @@
 
 TEST(FixedStringBuilderCase, MainTest) {
   std::tr1::array<char, 100> buffer;
-  iv::core::FixedStringBuilder builder(buffer.data(), buffer.size());
-  builder.AddCharacter('T');
-  builder.AddCharacter('H');
-  builder.AddCharacter('I');
-  builder.AddCharacter('S');
-  builder.AddString(" IS ");
-  builder.AddString("TEST");
-  EXPECT_STREQ("THIS IS TEST", builder.Finalize());
+  {
+    buffer.assign('\0');
+    iv::core::FixedStringBuilder builder(buffer.data(), buffer.size());
+    builder.AddCharacter('T');
+    builder.AddCharacter('H');
+    builder.AddCharacter('I');
+    builder.AddCharacter('S');
+    builder.AddString(" IS ");
+    builder.AddString("TEST");
+    EXPECT_STREQ("THIS IS TEST", builder.Finalize());
+  }
+  {
+    buffer.assign('\0');
+    iv::core::FixedStringBuilder builder(buffer.data(), buffer.size());
+    builder.AddPading('T', 20);
+    EXPECT_STREQ("TTTTTTTTTTTTTTTTTTTT", builder.Finalize());
+  }
+  {
+    buffer.assign('\0');
+    iv::core::FixedStringBuilder builder(buffer.data(), buffer.size());
+    builder.AddInteger(20000);
+    EXPECT_STREQ("20000", builder.Finalize());
+  }
+  {
+    buffer.assign('\0');
+    iv::core::FixedStringBuilder builder(buffer.data(), buffer.size());
+    builder.AddSubstring("TESTING", 4);
+    EXPECT_STREQ("TEST", builder.Finalize());
+  }
+
 }
