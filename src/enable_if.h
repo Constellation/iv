16490349ece1855f93136156629d1e5f66dd73dd
Constellation <utatane.tea@gmail.com>
2011-05-19 02:29:03 +0900

    add disable_if

--- src/enable_if.h
+++ src/enable_if.h
@@ -10,9 +10,23 @@ template<class T>
 struct enable_if_c<false, T> {
   // NO TYPEDEF!
 };
+
 template<class Cond, class T = void>
-struct enable_if : public enable_if_c<Cond::value, T> {
+struct enable_if : public enable_if_c<Cond::value, T> { };
+
+template<bool B, class T = void>
+struct disable_if_c {
+  typedef T type;
 };
+
+template<typename T>
+struct disable_if_c<true, T> {
+  // NO TYPEDEF!
+};
+
+template<class Cond, class T = void>
+struct disable_if : public disable_if_c<Cond::value, T> { };
+
 
 }  // namespace iv
 #endif  // _IV_ENABLE_IF_H_
