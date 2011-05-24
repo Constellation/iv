58fc65c623be51b5a19e4c06fc1df0cc6a354a5c
Constellation <utatane.tea@gmail.com>
2011-05-24 20:59:33 +0900

    fix include dependency

--- src/lv5/railgun_runtime.h
+++ src/lv5/railgun_runtime.h
@@ -1,7 +1,7 @@
 #ifndef _IV_LV5_RUNTIME_RAILGUN_H_
 #define _IV_LV5_RUNTIME_RAILGUN_H_
 #include "lv5/error_check.h"
-#include "lv5/railgun.h"
+#include "lv5/railgun_fwd.h"
 #include "lv5/internal.h"
 namespace iv {
 namespace lv5 {
