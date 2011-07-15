3fc7d181a7713db656ea762644c4bcbae0c5f8ec
Constellation <utatane.tea@gmail.com>
2011-07-15 18:19:55 +0900

    add ERROR GUARD macros

--- src/lv5/error_check.h
+++ src/lv5/error_check.h
@@ -1,20 +1,29 @@
 #ifndef _IV_LV5_ERROR_CHECK_H_
 #define _IV_LV5_ERROR_CHECK_H_
 
-#define IV_LV5_ERROR_VOID(error)\
-  error);\
+#define IV_LV5_ERROR_GUARD_VOID(error)\
   if (*error) {\
     return;\
-  }\
+  }
+
+#define IV_LV5_ERROR_GUARD_WITH(error, val)\
+  if (*error) {\
+    return val;\
+  }
+
+#define IV_LV5_ERROR_GUARD(error)\
+  IV_LV5_ERROR_GUARD_WITH(error, JSUndefined)
+
+#define IV_LV5_ERROR_VOID(error)\
+  error);\
+  IV_LV5_ERROR_GUARD_VOID(error)\
   ((void)0
 #define DUMMY )  // to make indentation work
 #undef DUMMY
 
 #define IV_LV5_ERROR_WITH(error, val)\
   error);\
-  if (*error) {\
-    return val;\
-  }\
+  IV_LV5_ERROR_GUARD_WITH(error, val)\
   ((void)0
 #define DUMMY )  // to make indentation work
 #undef DUMMY
