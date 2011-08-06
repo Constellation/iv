82b12aa7c7152c0a1a8555b27a6836fdc16dd7ad
Constellation <utatane.tea@gmail.com>
2011-08-07 05:01:25 +0900

    check target_ is not NULL   because, new CoreData and new Target is not the same time

--- src/lv5/railgun/core_data.h
+++ src/lv5/railgun/core_data.h
@@ -15,25 +15,27 @@ GC_ms_entry* CoreData::MarkChildren(GC_word* top,
                            entry, mark_sp_limit, reinterpret_cast<void**>(this));
   entry = GC_MARK_AND_PUSH(targets_,
                            entry, mark_sp_limit, reinterpret_cast<void**>(this));
-  for (InstTargets::const_iterator it = targets_->begin(),
-       last = targets_->end(); it != last; ++it) {
-    // loop and search Map pointer operations
-    // check global opcodes
-    if (VM::IsOP<OP::LOAD_GLOBAL>(**it) ||
-        VM::IsOP<OP::STORE_GLOBAL>(**it) ||
-        VM::IsOP<OP::DELETE_GLOBAL>(**it) ||
-        VM::IsOP<OP::CALL_GLOBAL>(**it) ||
-        VM::IsOP<OP::INCREMENT_GLOBAL>(**it) ||
-        VM::IsOP<OP::DECREMENT_GLOBAL>(**it) ||
-        VM::IsOP<OP::POSTFIX_INCREMENT_GLOBAL>(**it) ||
-        VM::IsOP<OP::POSTFIX_DECREMENT_GLOBAL>(**it) ||
-        VM::IsOP<OP::TYPEOF_GLOBAL>(**it)) {
-      // OPCODE | SYM | MAP
-      entry = GC_MARK_AND_PUSH(
-          (*it + 2)->map,
-          entry, mark_sp_limit, reinterpret_cast<void**>(this));
+  if (targets_) {
+    for (InstTargets::const_iterator it = targets_->begin(),
+         last = targets_->end(); it != last; ++it) {
+      // loop and search Map pointer operations
+      // check global opcodes
+      if (VM::IsOP<OP::LOAD_GLOBAL>(**it) ||
+          VM::IsOP<OP::STORE_GLOBAL>(**it) ||
+          VM::IsOP<OP::DELETE_GLOBAL>(**it) ||
+          VM::IsOP<OP::CALL_GLOBAL>(**it) ||
+          VM::IsOP<OP::INCREMENT_GLOBAL>(**it) ||
+          VM::IsOP<OP::DECREMENT_GLOBAL>(**it) ||
+          VM::IsOP<OP::POSTFIX_INCREMENT_GLOBAL>(**it) ||
+          VM::IsOP<OP::POSTFIX_DECREMENT_GLOBAL>(**it) ||
+          VM::IsOP<OP::TYPEOF_GLOBAL>(**it)) {
+        // OPCODE | SYM | MAP
+        entry = GC_MARK_AND_PUSH(
+            (*it + 2)->map,
+            entry, mark_sp_limit, reinterpret_cast<void**>(this));
+      }
+      std::advance(it, (*it)->GetLength());
     }
-    std::advance(it, (*it)->GetLength());
   }
   return entry;
 }
