28b4f8cc0cee9a5b022b00e049ac47307b164465
Constellation <utatane.tea@gmail.com>
2011-08-27 23:21:16 +0900

    add atomic.h / ref_counted.h / thread_safe_ref_counted.h

---
+++ src/atomic.h
@@ -0,0 +1,40 @@
+#ifndef _IV_ATOMIC_H_
+#define _IV_ATOMIC_H_
+#include "platform.h"
+
+#if defined(IV_OS_WIN)
+#include <windows.h>
+#elif defined(IV_OS_MACOSX)
+#include <libkern/OSAtomic.h>
+#endif
+
+namespace iv {
+namespace core {
+
+#if defined(IV_COMPILER_GCC) || defined(IV_COMPILER_CLANG)
+inline int AtomicIncrement(volatile int* target) {
+  return __sync_add_and_fetch(target, 1);
+}
+inline int AtomicDecrement(volatile int* target) {
+  return __sync_sub_and_fetch(target, 1);
+}
+#elif defined(IV_OS_WIN)
+inline int AtomicIncrement(volatile int* target) {
+  return InterlockedIncrement(static_cast<volatile long*>(target));
+}
+inline int AtomicDecrement(volatile int* target) {
+  return InterlockedDecrement(static_cast<volatile long*>(target));
+}
+#elif defined(IV_OS_MACOSX)
+inline int AtomicIncrement(volatile int* target) {
+  return OSAtomicIncrement64Barrier(const_cast<int*>(target));
+}
+inline int AtomicDecrement(volatile int* target) {
+  return OSAtomicDecrement64Barrier(const_cast<int*>(target));
+}
+#else
+#error ATOMIC INCREMENT / DECREMENT NOT DEFINED in atomic.h
+#endif
+
+} }  // namespace iv::core
+#endif  // _IV_ATOMIC_H_
---
+++ test/test_thread_safe_ref_counted.cc
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+#include "thread_safe_ref_counted.h"
+namespace {
+
+class Check : public iv::core::ThreadSafeRefCounted<Check> {
+ public:
+  Check(bool* target) : target_(target) { }
+  ~Check() {
+    *target_ = true;
+  }
+ private:
+  bool* target_;
+};
+
+}  // namespace anonymous
+
+TEST(ThreadSafeRefCountedCase, ThreadSafeRefCountedTest) {
+  bool target = false;
+  Check* check = new Check(&target);
+  EXPECT_EQ(check->RetainCount(), 1);
+  check->Release();
+  EXPECT_TRUE(target);
+}
