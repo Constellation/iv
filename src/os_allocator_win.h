9330426ed7e7380e140d432beebc77a5a8222e60
Constellation <utatane.tea@gmail.com>
2011-09-14 01:23:40 +0900

    VirtualFree for MEM_RELEASE size must be 0

--- src/os_allocator_win.h
+++ src/os_allocator_win.h
@@ -9,7 +9,6 @@ inline void* OSAllocator::Allocate(std::size_t bytes) {
   return malloc(bytes);
 #else
   void* mem = VirtualAlloc(0, bytes, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
-printf("alloc mem=%p, size=%d\n", mem, bytes);
   if (!mem) {
     std::abort();
   }
@@ -21,9 +20,7 @@ inline void OSAllocator::Deallocate(void* address, std::size_t bytes) {
 #if 1
   free(address);
 #else
-printf("free addr=%p, size=%d\n", address, bytes);
-  const bool res = VirtualFree(address, bytes, MEM_RELEASE) != 0;
-  printf("last error=%d\n", ::GetLastError());
+  const bool res = VirtualFree(address, 0, MEM_RELEASE) != 0;
   if (!res) {
     std::abort();
   }
@@ -35,7 +32,6 @@ inline void OSAllocator::Commit(void* address, std::size_t bytes) {
   return;
 #else
   const void* mem = VirtualAlloc(address, bytes, MEM_COMMIT, PAGE_READWRITE);
-printf("commit mem=%p, addr=%p, byte=%d\n", mem, address, bytes);
   if (!mem) {
     std::abort();
   }
@@ -46,7 +42,6 @@ inline void OSAllocator::Decommit(void* address, std::size_t bytes) {
 #if 1
   return;
 #else
-printf("decomit addr=%p, byte=%d\n", address, bytes);
   const bool res = VirtualFree(address, bytes, MEM_DECOMMIT) != 0;
   if (!res) {
     std::abort();
