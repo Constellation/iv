b9f7e4cba0eea130ff82e5e1e55f48bb0110c63b
Constellation <utatane.tea@gmail.com>
2011-10-19 03:24:50 +0900

    add more test cases in aero #34

--- test/lv5/test_aero_exec.cc
+++ test/lv5/test_aero_exec.cc
@@ -34,7 +34,33 @@ TEST(AeroExecCase, MainTest) {
     ASSERT_FALSE(error);
     iv::lv5::aero::Compiler compiler(iv::lv5::aero::MULTILINE);
     iv::lv5::aero::Code code = compiler.Compile(data);
-    disasm.DisAssemble(code);
     ASSERT_TRUE(vm.ExecuteOnce(&code, str1, 0, vec.data()));
+  }
+  {
+    space.Clear();
+    iv::core::UString reg = iv::core::ToUString("b{0,93}c");
+    iv::core::UString str1 = iv::core::ToUString("aaabbbbcccddeeeefffff");
+    iv::lv5::aero::Parser parser(&space, reg, iv::lv5::aero::NONE);
+    int error = 0;
+    iv::lv5::aero::ParsedData data = parser.ParsePattern(&error);
+    ASSERT_FALSE(error);
+    iv::lv5::aero::Compiler compiler(iv::lv5::aero::NONE);
+    iv::lv5::aero::Code code = compiler.Compile(data);
+    ASSERT_TRUE(vm.ExecuteOnce(&code, str1, 0, vec.data()));
+    EXPECT_EQ(3, vec[0]);
+    EXPECT_EQ(8, vec[1]);
+  }
+  {
+    space.Clear();
+    iv::core::UString reg = iv::core::ToUString("\\b(\\w+) \\2\\b");
+    iv::core::UString str1 = iv::core::ToUString("do you listen the the band");
+    iv::lv5::aero::Parser parser(&space, reg, iv::lv5::aero::NONE);
+    int error = 0;
+    iv::lv5::aero::ParsedData data = parser.ParsePattern(&error);
+    ASSERT_FALSE(error);
+    iv::lv5::aero::Compiler compiler(iv::lv5::aero::NONE);
+    iv::lv5::aero::Code code = compiler.Compile(data);
+    // disasm.DisAssemble(code);
+    // ASSERT_FALSE(vm.ExecuteOnce(&code, str1, 0, vec.data()));
   }
 }
