dd52ba8fe7e0d32451f9a1daeda02fe0094e751a
Constellation <utatane.tea@gmail.com>
2011-10-21 02:11:35 +0900

    add JSONQuote control character test

--- test/test_conversions.cc
+++ test/test_conversions.cc
@@ -332,3 +332,47 @@ TEST(ConversionsCase, BigRadix) {
     EXPECT_EQ(std::numeric_limits<double>::infinity(), iv::core::StringToIntegerWithRadix(str, 16, true));
   }
 }
+
+TEST(ConversionsCase, JSONQuote) {
+  std::array<const char*, 32> expected = { {
+    "\\u0000",
+    "\\u0001",
+    "\\u0002",
+    "\\u0003",
+    "\\u0004",
+    "\\u0005",
+    "\\u0006",
+    "\\u0007",
+    "\\b",
+    "\\t",
+    "\\n",
+    "\\u000b",
+    "\\f",
+    "\\r",
+    "\\u000e",
+    "\\u000f",
+    "\\u0010",
+    "\\u0011",
+    "\\u0012",
+    "\\u0013",
+    "\\u0014",
+    "\\u0015",
+    "\\u0016",
+    "\\u0017",
+    "\\u0018",
+    "\\u0019",
+    "\\u001a",
+    "\\u001b",
+    "\\u001c",
+    "\\u001d",
+    "\\u001e",
+    "\\u001f"
+  } };
+  std::vector<char> res;
+  for (uint16_t i = 0; i < 32; ++i) {
+    res.clear();
+    iv::core::JSONQuote(&i, &i + 1, std::back_inserter(res));
+    res.push_back('\0');
+    EXPECT_STREQ(expected[i], res.data());
+  }
+}
