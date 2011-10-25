ec0492e6acb6d233e05e27ec0d58dbd41a2d3ce5
Constellation <utatane.tea@gmail.com>
2011-10-25 21:40:47 +0900

    add NTZ64

--- src/arith.h
+++ src/arith.h
@@ -1,6 +1,7 @@
 // arithmetic functions
 #ifndef IV_ARITH_H_
 #define IV_ARITH_H_
+#include "detail/cinttypes.h"
 namespace iv {
 namespace core {
 
@@ -46,6 +47,53 @@ inline uint32_t CLP2(uint32_t x) {
   x = x | (x >> 16);
   return x + 1;
 }
+
+// NTZ: number of tailing zeros
+// faster way than "Hacker's Delight" section 5-4
+// http://www-graphics.stanford.edu/~seander/bithacks.html#ZerosOnRightMultLookup
+// http://slashdot.jp/~Tellur52/journal/448479
+// http://d.hatena.ne.jp/siokoshou/20090704#p1
+//
+// using perfect hashing
+//
+// table creation code is following
+//   #include <cstdint>
+//   #include <iostream>
+//   int main(int argc, char** argv) {
+//     static int* table = new int[ 64 ];
+//     uint64_t hash = 0x03F566ED27179461UL;
+//     for (int i = 0; i < 64; ++i) {
+//       table[hash >> 58] = i;
+//       hash <<= 1;
+//     }
+//     for (int i = 0; i < 64; i++) {
+//       std::cout << table[i];
+//       if (i % 16 != 15) {
+//         std::cout << ", ";
+//       } else if (i == 63) {
+//         std::cout << std::endl;
+//       } else {
+//         std::cout << "," << std::endl;
+//       }
+//     }
+//     std::cout << std::endl;
+//     return 0;
+//   }
+static const std::array<uint8_t, 64> kNTZ64 = { {
+  0,   1, 59,  2, 60, 40, 54,  3, 61, 32, 49, 41, 55, 19, 35,  4,
+  62, 52, 30, 33, 50, 12, 14, 42, 56, 16, 27, 20, 36, 23, 44,  5,
+  63, 58, 39, 53, 31, 48, 18, 34, 51, 29, 11, 13, 15, 26, 22, 43,
+  57, 38, 47, 17, 28, 10, 25, 21, 37, 46,  9, 24, 45,  8,  7,  6
+} };
+
+inline uint64_t NTZ64(int64_t x) {
+  if (!x) {
+    return 64;
+  }
+  return kNTZ64[
+      static_cast<uint64_t>(x & -x) * UINT64_C(0x03F566ED27179461) >> 58];
+}
+
 
 } }  // namespace iv::core
 #endif  // IV_ARITH_H_
