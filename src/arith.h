962146a7beeb483d69fb1c0ca116384da077b02b
Constellation <utatane.tea@gmail.com>
2011-10-27 00:28:42 +0900

    add static NTZ

--- src/arith.h
+++ src/arith.h
@@ -40,6 +40,31 @@ struct FLP2 {
   static const uint32_t value = x - (x >> 1);
 };
 
+template<uint32_t N>
+struct NTZTable;
+
+#define V(I, N)\
+  template<>struct NTZTable<I>{\
+    static const int value = N;\
+  };
+#define NTZ_TABLE(V)\
+V( 0,  0) V( 1,  1) V( 2, 59) V( 3,  2) V( 4, 60) V( 5, 40) V( 6, 54) V( 7,  3)\
+V( 8, 61) V( 9, 32) V(10, 49) V(11, 41) V(12, 55) V(13, 19) V(14, 35) V(15,  4)\
+V(16, 62) V(17, 52) V(18, 30) V(19, 33) V(20, 50) V(21, 12) V(22, 14) V(23, 42)\
+V(24, 56) V(25, 16) V(26, 27) V(27, 20) V(28, 36) V(29, 23) V(30, 44) V(31,  5)\
+V(32, 63) V(33, 58) V(34, 39) V(35, 53) V(36, 31) V(37, 48) V(38, 18) V(39, 34)\
+V(40, 51) V(41, 29) V(42, 11) V(43, 13) V(44, 15) V(45, 26) V(46, 22) V(47, 43)\
+V(48, 57) V(49, 38) V(50, 47) V(51, 17) V(52, 28) V(53, 10) V(54, 25) V(55, 21)\
+V(56, 37) V(57, 46) V(58,  9) V(59, 24) V(60, 45) V(61,  8) V(62,  7) V(63,  6)
+NTZ_TABLE(V)
+#undef V
+#undef NTZ_TABLE
+
+template<int64_t X>
+struct NTZ {
+  static const int value = (!X) ? 64 : NTZTable<(static_cast<uint64_t>(X & -X) * UINT64_C(0x03F566ED27179461) >> 58)>::value;
+};
+
 }  // namespace detail
 
 // from "Hacker's Delight" section 2-12
