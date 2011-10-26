0d83ff24740bfdd2f83618bfeb4336aa5e89b99d
Constellation <utatane.tea@gmail.com>
2011-10-26 10:41:49 +0900

    add no_operator_names_gaurd.h

---
+++ src/no_operator_names_gaurd.h
@@ -0,0 +1,9 @@
+#ifndef IV_NO_OPERATOR_NAMES_GUARD_H_
+#define IV_NO_OPERATOR_NAMES_GUARD_H_
+// http://homepage1.nifty.com/herumi/diary/1109.html#7
+
+#if not +0
+#error "operator names used. use -fno-operator-names"
+#endif
+
+#endif  // IV_NO_OPERATOR_NAMES_GUARD_H_
