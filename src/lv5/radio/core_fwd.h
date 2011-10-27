d7f41fb3110dcc27310db82cba7da7f828a740e3
Constellation <utatane.tea@gmail.com>
2011-10-28 01:52:34 +0900

    add static assert for alignment

--- src/lv5/radio/core_fwd.h
+++ src/lv5/radio/core_fwd.h
@@ -37,6 +37,7 @@ class Core : private core::Noncopyable<Core> {
   Cell* Allocate() {
     typedef std::is_base_of<Cell, T> cond;
     IV_STATIC_ASSERT(cond::value);
+    IV_STATIC_ASSERT(AlignOf(T) <= kBlockControlStep);
     return AllocateFrom(
         GetBlockControl<IV_ROUNDUP(sizeof(T), kBlockControlStep)>());
   }
