a553a46a0f2832156b5f956457ebe3a75cdb1327
Constellation <utatane.tea@gmail.com>
2011-12-12 01:10:20 +0900

    fix including type_traits

--- iv/conversions.h
+++ iv/conversions.h
@@ -9,6 +9,7 @@
 #include <iv/detail/array.h>
 #include <iv/detail/cstdint.h>
 #include <iv/detail/cinttypes.h>
+#include <iv/detail/type_traits.h>
 #include <iv/platform_math.h>
 #include <iv/canonicalized_nan.h>
 #include <iv/character.h>
@@ -324,7 +325,7 @@ inline OutputIter IntToString(IntT integer, OutputIter res) {
   //
   // -INT32_MIN / - INT64_MIN is overflowed,
   // treat negative / positive separately
-  // IV_STATIC_ASSERT(std::is_signed<IntT>::value);
+  IV_STATIC_ASSERT(std::is_signed<IntT>::value);
   std::array<char, BufferWidth<IntT>::value> buf;
   int integer_pos = buf.size();
   if (integer >= 0) {
@@ -349,7 +350,7 @@ inline OutputIter UIntToString(UIntT integer, OutputIter res) {
   //  => 10
   // UINT64_MAX => 18446744073709551615
   //  => 20
-  // IV_STATIC_ASSERT(std::is_unsigned<UIntT>::value);
+  IV_STATIC_ASSERT(std::is_unsigned<UIntT>::value);
   std::array<char, BufferWidth<UIntT>::value> buf;
   int integer_pos = buf.size();
   do {
