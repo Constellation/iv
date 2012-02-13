9d6b78ae7617ebb9cadf12fbdf883d6245bb2050
Constellation <utatane.tea@gmail.com>
2012-02-13 13:27:07 +0900

    implement LRUCodeMap

--- iv/lv5/railgun/context_fwd.h
+++ iv/lv5/railgun/context_fwd.h
@@ -2,6 +2,7 @@
 #define IV_LV5_RAILGUN_CONTEXT_FWD_H_
 #include <gc/gc_cpp.h>
 #include <iv/lv5/railgun/fwd.h>
+#include <iv/lv5/railgun/lru_code_map.h>
 namespace iv {
 namespace lv5 {
 namespace railgun {
@@ -14,10 +15,14 @@ class Context : public lv5::Context {
   VM* vm() {
     return vm_;
   }
+
+  LRUCodeMap* direct_eval_map() {
+    return &direct_eval_map_;
+  }
  private:
   VM* vm_;
+  LRUCodeMap direct_eval_map_;
 };
-
 
 } } }  // namespace iv::lv5::railgun
 #endif  // IV_LV5_RAILGUN_CONTEXT_FWD_H_
