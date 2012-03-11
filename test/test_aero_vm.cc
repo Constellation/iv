ec8bc44f3fffdcbba9fe66d474469252c1a5e0b9
Constellation <utatane.tea@gmail.com>
2012-03-11 22:53:31 +0900

    update test_aero_vm

--- test/test_aero_vm.cc
+++ test/test_aero_vm.cc
@@ -273,48 +273,6 @@ TEST(AeroVMCase, CaptureTest) {
   }
   {
     space.Clear();
-    iv::core::UString reg = iv::core::ToUString("(z)((a+)?(b+)?(c))*");
-    iv::core::UString str1 = iv::core::ToUString("zaacbbbcac");
-    iv::aero::Parser<iv::core::UStringPiece> parser(&space, reg, iv::aero::NONE);
-    int error = 0;
-    iv::aero::ParsedData data = parser.ParsePattern(&error);
-    ASSERT_FALSE(error);
-    iv::aero::Compiler compiler(iv::aero::NONE);
-    iv::core::ScopedPtr<iv::aero::Code> code(compiler.Compile(data));;
-    // disasm.DisAssemble(code);
-    ASSERT_TRUE(vm.Execute(code.get(), str1, vec.data(), 0));
-    EXPECT_EQ(0,  vec[0]);
-    EXPECT_EQ(10, vec[1]);
-    EXPECT_EQ(0,  vec[2]);
-    EXPECT_EQ(1,  vec[3]);
-    EXPECT_EQ(8,  vec[4]);
-    EXPECT_EQ(10, vec[5]);
-    EXPECT_EQ(8,  vec[6]);
-    EXPECT_EQ(9,  vec[7]);
-    EXPECT_EQ(-1, vec[8]);
-    EXPECT_EQ(-1, vec[9]);
-    EXPECT_EQ(9,  vec[10]);
-    EXPECT_EQ(10, vec[11]);
-  }
-  {
-    space.Clear();
-    iv::core::UString reg = iv::core::ToUString("(aa|aabaac|ba|b|c)*");
-    iv::core::UString str1 = iv::core::ToUString("aabaac");
-    iv::aero::Parser<iv::core::UStringPiece> parser(&space, reg, iv::aero::NONE);
-    int error = 0;
-    iv::aero::ParsedData data = parser.ParsePattern(&error);
-    ASSERT_FALSE(error);
-    iv::aero::Compiler compiler(iv::aero::NONE);
-    iv::core::ScopedPtr<iv::aero::Code> code(compiler.Compile(data));;
-    // disasm.DisAssemble(code);
-    ASSERT_TRUE(vm.Execute(code.get(), str1, vec.data(), 0));
-    EXPECT_EQ(0, vec[0]);
-    EXPECT_EQ(4, vec[1]);
-    EXPECT_EQ(2, vec[2]);
-    EXPECT_EQ(4, vec[3]);
-  }
-  {
-    space.Clear();
     iv::core::UString reg = iv::core::ToUString("a[a-z]{2,4}?");
     iv::core::UString str1 = iv::core::ToUString("abcdefghi");
     iv::aero::Parser<iv::core::UStringPiece> parser(&space, reg, iv::aero::NONE);
@@ -451,35 +409,6 @@ TEST(AeroVMCase, CaptureTest) {
   }
   {
     space.Clear();
-    iv::core::UString reg = iv::core::ToUString(kURLRegExp);
-    iv::core::UString str1 = iv::core::ToUString("http://github.com/Constellation/");
-    iv::aero::Parser<iv::core::UStringPiece> parser(&space, reg, iv::aero::NONE);
-    int error = 0;
-    iv::aero::ParsedData data = parser.ParsePattern(&error);
-    ASSERT_FALSE(error);
-    iv::aero::Compiler compiler(iv::aero::NONE);
-    iv::core::ScopedPtr<iv::aero::Code> code(compiler.Compile(data));;
-    // disasm.DisAssemble(code);
-    ASSERT_TRUE(vm.Execute(code.get(), str1, vec.data(), 0));
-    EXPECT_EQ(0, vec[0]);
-    EXPECT_EQ(32, vec[1]);
-    EXPECT_EQ(7, vec[2]);
-    EXPECT_EQ(17, vec[3]);
-    EXPECT_EQ(7, vec[4]);
-    EXPECT_EQ(14, vec[5]);
-    EXPECT_EQ(7, vec[6]);
-    EXPECT_EQ(13, vec[7]);
-    EXPECT_EQ(14, vec[8]);
-    EXPECT_EQ(17, vec[9]);
-    EXPECT_EQ(-1, vec[10]);
-    EXPECT_EQ(-1, vec[11]);
-    EXPECT_EQ(17, vec[12]);
-    EXPECT_EQ(32, vec[13]);
-    EXPECT_EQ(18, vec[14]);
-    EXPECT_EQ(31, vec[15]);
-  }
-  {
-    space.Clear();
     iv::core::UString reg = iv::core::ToUString("[a-z][^1-9][a-z]");
     iv::core::UString str1 = iv::core::ToUString("def");
     iv::aero::Parser<iv::core::UStringPiece> parser(&space, reg, iv::aero::NONE);
@@ -522,5 +451,83 @@ TEST(AeroVMCase, CaptureTest) {
     ASSERT_TRUE(vm.Execute(code.get(), str1, vec.data(), 0));
     EXPECT_EQ(0, vec[0]);
     EXPECT_EQ(3, vec[1]);
+  }
+}
+
+TEST(AeroVMCase, CaptureTest2) {
+  iv::core::Space space;
+  iv::aero::VM vm;
+  std::vector<int> vec(1000);
+  iv::aero::OutputDisAssembler disasm(stdout);
+  {
+    space.Clear();
+    iv::core::UString reg = iv::core::ToUString("(z)((a+)?(b+)?(c))*");
+    iv::core::UString str1 = iv::core::ToUString("zaacbbbcac");
+    iv::aero::Parser<iv::core::UStringPiece> parser(&space, reg, iv::aero::NONE);
+    int error = 0;
+    iv::aero::ParsedData data = parser.ParsePattern(&error);
+    ASSERT_FALSE(error);
+    iv::aero::Compiler compiler(iv::aero::NONE);
+    iv::core::ScopedPtr<iv::aero::Code> code(compiler.Compile(data));;
+    // disasm.DisAssemble(code);
+    ASSERT_TRUE(vm.Execute(code.get(), str1, vec.data(), 0));
+    EXPECT_EQ(0,  vec[0]);
+    EXPECT_EQ(10, vec[1]);
+    EXPECT_EQ(0,  vec[2]);
+    EXPECT_EQ(1,  vec[3]);
+    EXPECT_EQ(8,  vec[4]);
+    EXPECT_EQ(10, vec[5]);
+    EXPECT_EQ(8,  vec[6]);
+    EXPECT_EQ(9,  vec[7]);
+    EXPECT_EQ(-1, vec[8]);
+    EXPECT_EQ(-1, vec[9]);
+    EXPECT_EQ(9,  vec[10]);
+    EXPECT_EQ(10, vec[11]);
+  }
+  {
+    space.Clear();
+    iv::core::UString reg = iv::core::ToUString("(aa|aabaac|ba|b|c)*");
+    iv::core::UString str1 = iv::core::ToUString("aabaac");
+    iv::aero::Parser<iv::core::UStringPiece> parser(&space, reg, iv::aero::NONE);
+    int error = 0;
+    iv::aero::ParsedData data = parser.ParsePattern(&error);
+    ASSERT_FALSE(error);
+    iv::aero::Compiler compiler(iv::aero::NONE);
+    iv::core::ScopedPtr<iv::aero::Code> code(compiler.Compile(data));;
+    // disasm.DisAssemble(code);
+    ASSERT_TRUE(vm.Execute(code.get(), str1, vec.data(), 0));
+    EXPECT_EQ(0, vec[0]);
+    EXPECT_EQ(4, vec[1]);
+    EXPECT_EQ(2, vec[2]);
+    EXPECT_EQ(4, vec[3]);
+  }
+  {
+    space.Clear();
+    iv::core::UString reg = iv::core::ToUString(kURLRegExp);
+    iv::core::UString str1 = iv::core::ToUString("http://github.com/Constellation/");
+    iv::aero::Parser<iv::core::UStringPiece> parser(&space, reg, iv::aero::NONE);
+    int error = 0;
+    iv::aero::ParsedData data = parser.ParsePattern(&error);
+    ASSERT_FALSE(error);
+    iv::aero::Compiler compiler(iv::aero::NONE);
+    iv::core::ScopedPtr<iv::aero::Code> code(compiler.Compile(data));;
+    // disasm.DisAssemble(code);
+    ASSERT_TRUE(vm.Execute(code.get(), str1, vec.data(), 0));
+    EXPECT_EQ(0, vec[0]);
+    EXPECT_EQ(32, vec[1]);
+    EXPECT_EQ(7, vec[2]);
+    EXPECT_EQ(17, vec[3]);
+    EXPECT_EQ(7, vec[4]);
+    EXPECT_EQ(14, vec[5]);
+    EXPECT_EQ(7, vec[6]);
+    EXPECT_EQ(13, vec[7]);
+    EXPECT_EQ(14, vec[8]);
+    EXPECT_EQ(17, vec[9]);
+    EXPECT_EQ(-1, vec[10]);
+    EXPECT_EQ(-1, vec[11]);
+    EXPECT_EQ(17, vec[12]);
+    EXPECT_EQ(32, vec[13]);
+    EXPECT_EQ(18, vec[14]);
+    EXPECT_EQ(31, vec[15]);
   }
 }
