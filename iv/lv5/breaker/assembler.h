0d0e6da0bf08411f8f06cd4f3ff3cea97651a12d
Constellation <utatane.tea@gmail.com>
2012-04-13 21:46:27 +0900

    add breaker/assembler template

---
+++ iv/lv5/breaker/assembler.h
@@ -0,0 +1,12 @@
+#ifndef IV_LV5_BREAKER_ASSEMBLER_H_
+#define IV_LV5_BREAKER_ASSEMBLER_H_
+#include <iv/lv5/breaker/fwd.h>
+namespace iv {
+namespace lv5 {
+namespace breaker {
+
+class Assembler : public Xbyak::CodeGenerator {
+};
+
+} } }  // namespace iv::lv5::breaker
+#endif  // IV_LV5_BREAKER_ASSEMBLER_H_
