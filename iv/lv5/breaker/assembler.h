0eb687f7d8069bff64ed6550a04f22bba4b7d7fa
Constellation <utatane.tea@gmail.com>
2012-04-14 19:11:32 +0900

    add scan jump phase

--- iv/lv5/breaker/assembler.h
+++ iv/lv5/breaker/assembler.h
@@ -1,11 +1,35 @@
 #ifndef IV_LV5_BREAKER_ASSEMBLER_H_
 #define IV_LV5_BREAKER_ASSEMBLER_H_
 #include <iv/lv5/breaker/fwd.h>
+#include <iv/noncopyable.h>
 namespace iv {
 namespace lv5 {
 namespace breaker {
 
 class Assembler : public Xbyak::CodeGenerator {
+ public:
+  class LocalLabelScope : core::Noncopyable<> {
+   public:
+    LocalLabelScope(Assembler* assembler)
+      : assembler_(assembler) {
+      assembler_->inLocalLabel();
+    }
+    ~LocalLabelScope() {
+      assembler_->outLocalLabel();
+    }
+   private:
+    Assembler* assembler_;
+  };
+
+  Assembler()
+    : Xbyak::CodeGenerator(4096, Xbyak::AutoGrow) {
+  }
+
+  Executable GainExecutableByOffset(std::size_t offset) const {
+    return reinterpret_cast<Executable>(getCode() + offset);
+  }
+
+  std::size_t size() const { return getSize(); }
 };
 
 } } }  // namespace iv::lv5::breaker
--- iv/lv5/breaker/compiler.h
+++ iv/lv5/breaker/compiler.h
@@ -10,23 +10,113 @@
 #define IV_LV5_BREAKER_COMPILER_H_
 #include <iv/lv5/breaker/fwd.h>
 #include <iv/lv5/railgun/railgun.h>
+#include <iv/lv5/breaker/executable.h>
+#include <iv/lv5/breaker/assembler.h>
 namespace iv {
 namespace lv5 {
 namespace breaker {
 
 class Compiler {
  public:
+  typedef std::unordered_map<railgun::Code*, std::size_t> EntryPointMap;
+  typedef std::unordered_map<uint32_t, std::size_t> JumpMap;
   Compiler()
-    : code_(NULL) {
+    : code_(NULL),
+      asm_(new (PointerFree) Assembler),
+      entry_points_() {
+  }
+
+  ~Compiler() {
+    asm_->ready();
+    for (EntryPointMap::const_iterator it = entry_points_.begin(),
+         last = entry_points_.end(); it != last; ++it) {
+      it->first->set_executable(asm_->GainExecutableByOffset(it->second));
+    }
   }
 
   void Initialize(railgun::Code* code) {
     code_ = code;
+    jump_map_.clear();
+    entry_points_.insert(std::make_pair(code, asm_->size()));
   }
 
   void Compile(railgun::Code* code) {
     Initialize(code);
+    ScanJumps();
+    Main();
   }
+
+  // scan jump target for setting labels
+  void ScanJumps() {
+    // jump opcodes
+    //   IF_FALSE
+    //   IF_TRUE
+    //   JUMP_SUBROUTINE
+    //   JUMP_BY
+    //   FORIN_SETUP
+    //   FORIN_ENUMERATE
+    namespace r = railgun;
+
+    const r::Instruction* first_instr = code_->begin();
+    for (const r::Instruction* instr = code_->begin(),
+         *last = code_->end(); instr != last;) {
+      const uint32_t opcode = instr->GetOP();
+      const uint32_t length = r::kOPLength[opcode];
+      const int32_t index = instr - first_instr;
+      switch (opcode) {
+        case r::OP::IF_FALSE:
+        case r::OP::IF_TRUE:
+        case r::OP::JUMP_SUBROUTINE:
+        case r::OP::JUMP_BY:
+        case r::OP::FORIN_SETUP:
+        case r::OP::FORIN_ENUMERATE: {
+          const int32_t jump = instr[1].jump.to;
+          const uint32_t to = index + jump;
+          jump_map_.insert(std::make_pair(to, 0));
+          break;
+        }
+      }
+      std::advance(it, length);
+    }
+
+    // and handlers table
+    const ExceptionTable& table = code_->exception_table();
+    for (ExceptionTable::const_iterator it = table.begin(),
+         last = table.end(); it != last; ++it) {
+      const Handler& handler = *it;
+      jump_map_.insert(std::make_pair(handler.begin(), 0));
+      jump_map_.insert(std::make_pair(handler.end(), 0));
+    }
+  }
+
+  void Main() {
+    namespace r = railgun;
+
+    // generate local label scope
+    const Assembler::LocalLabelScope local_label_scope(asm_);
+
+    const r::Instruction* first_instr = code_->begin();
+    for (const r::Instruction* instr = code_->begin(),
+         *last = code_->end(); instr != last;) {
+      const uint32_t opcode = instr->GetOP();
+      const uint32_t length = r::kOPLength[opcode];
+      const int32_t index = instr - first_instr;
+
+      const JumpMap::const_iterator it = jump_map_.find(index);
+      if (it != jump_map_.end()) {
+        it->second = asm_->size();
+      }
+
+      switch (opcode) {
+      }
+      std::advance(it, length);
+    }
+  }
+
+  railgun::Code* code_;
+  Assembler* asm_;
+  JumpMap jump_map_;
+  EntryPointMap entry_points_;
 };
 
 inline void CompileInternal(Compiler* compiler, railgun::Code* code) {
