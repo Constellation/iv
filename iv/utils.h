ad2589be4a8c6c5af37c121b35e4eb5710abb841
Constellation <utatane.tea@gmail.com>
2012-04-19 18:38:39 +0900

    add IV_OFFSETOF macro

--- iv/utils.h
+++ iv/utils.h
@@ -47,6 +47,18 @@ class AlignOfImpl {
 // only 2^n and unsinged
 #define IV_ROUNDDOWN(x, y) ((x) & (-(y)))
 
+// OFFSETOF for C++ classes (non-POD)
+// http://www.kijineko.co.jp/tech/cpptempls/struct/offsetof.html
+// This macro purges '&' operator override,
+// and return offset value of non-POD class.
+// IV_DUMMY provides dummy address, that is not used in other places.
+void IV_DUMMY();
+#define IV_DUMMY_ADDRESS ((char*)&::iv::core::IV_DUMMY)  /* NOLINT */
+#define IV_OFFSETOF(type, member)\
+/* NOLINT */    (((char*)(\
+/* NOLINT */      &reinterpret_cast<char const volatile&>(\
+/* NOLINT */        ((type*)IV_DUMMY_ADDRESS)->member))) - IV_DUMMY_ADDRESS)
+
 template<class T>
 T LowestOneBit(T value) {
   return value & (~value + 1u);
