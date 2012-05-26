08cc49c6e1b17f7a75c72ce27a49829836a241d8
Constellation <utatane.tea@gmail.com>
2012-05-26 23:20:22 +0900

    Implement CoreData#AttachLine

--- iv/lv5/railgun/core_data_fwd.h
+++ iv/lv5/railgun/core_data_fwd.h
@@ -1,6 +1,8 @@
 #ifndef IV_LV5_RAILGUN_CORE_DATA_FWD_H_
 #define IV_LV5_RAILGUN_CORE_DATA_FWD_H_
+#include <algorithm>
 #include <iv/detail/unique_ptr.h>
+#include <iv/sorted_vector.h>
 #include <iv/lv5/gc_template.h>
 #include <iv/lv5/gc_kind.h>
 #include <iv/lv5/railgun/op.h>
@@ -45,7 +47,50 @@ class CoreData : public GCKind<CoreData> {
     compiled_ = true;
   }
 
+  struct Comparator {
+    bool operator()(const BytecodeOffsetAndLine& line,
+                    std::size_t bytecode_offset) const {
+      return line.first < bytecode_offset;
+    }
+
+    bool operator()(std::size_t bytecode_offset,
+                    const BytecodeOffsetAndLine& line) const {
+      return bytecode_offset < line.first;
+    }
+  };
+
   void AttachLine(std::size_t bytecode_offset, std::size_t line_number) {
+    // This code is valid because Lines is vector.
+    const Lines::iterator it =
+        std::lower_bound(lines_.begin(), lines_.end(),
+                         bytecode_offset, Comparator());
+    if (it != lines_.end()) {
+      if (it->first == bytecode_offset) {
+        if (it->second > line_number) {
+          *it = std::make_pair(it->first, line_number);
+        }
+        return;
+      }
+    }
+
+    if (it != lines_.begin()) {
+      const Lines::iterator prev = it - 1;
+      if (prev->second == line_number) {
+        return;
+      }
+    }
+    lines_.insert(std::make_pair(bytecode_offset, line_number));
+  }
+
+  std::size_t LookupLineNumber(std::size_t bytecode_offset) const {
+    const Lines::const_iterator it =
+        std::upper_bound(lines_.begin(),
+                         lines_.end(),
+                         bytecode_offset, Comparator());
+    if (it != lines_.begin()) {
+      return (it - 1)->second;
+    }
+    return 1;
   }
 
  private:
