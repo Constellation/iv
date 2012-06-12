914fa37d620b8565551e135718e8d9d2f58e9e65
Constellation <utatane.tea@gmail.com>
2012-06-12 17:43:34 +0900

    Add lhs test cases to lhs-assignment

--- test/lv5/suite/spec/lhs-assignment.js
+++ test/lv5/suite/spec/lhs-assignment.js
@@ -2,7 +2,8 @@ describe("lhs assignment", function() {
   it("literal assignment", function() {
     var i = 0;
     try {
-      eval('i = 10 = 20');
+      // should be early error
+      eval('(function () { i = 10 = 20 }()');
     } catch (e) {
       expect(i).toBe(0);
     }
@@ -10,10 +11,22 @@ describe("lhs assignment", function() {
 
   it("literal assignment with +", function() {
     var i = 0;
-    var obj = {
-    };
     try {
-      eval('i = 10 += 20');
+      // should be early error
+      eval('(function () { i = 10 += 20 }()');
+    } catch (e) {
+      expect(i).toBe(0);
+    }
+  });
+
+  it("assignment to function call", function() {
+    expect(eval('(function () { print() = 10; }); true')).toBe(true);
+  });
+
+  it("assignment to constructor call", function() {
+    var i = 0;
+    try {
+      eval('(function () { new print() = 10; });');
     } catch (e) {
       expect(i).toBe(0);
     }
