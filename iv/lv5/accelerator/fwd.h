974ae1c840d9189290c664fa8a16f1270ade6073
Constellation <utatane.tea@gmail.com>
2012-07-06 23:48:31 +0900

    Add accelerator directory

---
+++ iv/lv5/accelerator/fwd.h
@@ -0,0 +1,8 @@
+#ifndef IV_LV5_ACCELERATOR_FWD_H_
+#define IV_LV5_ACCELERATOR_FWD_H_
+namespace iv {
+namespace lv5 {
+namespace accelerator {
+
+} } }  // namespace iv::lv5::accelerator
+#endif  // IV_LV5_ACCELERATOR_FWD_H_
