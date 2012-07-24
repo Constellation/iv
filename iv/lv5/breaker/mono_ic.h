7f5209ef7f14dee23044de2c9aa738b646271004
Constellation <utatane.tea@gmail.com>
2012-07-24 22:08:03 +0900

    Add initial MonoIC compiler file

---
+++ iv/lv5/breaker/mono_ic.h
@@ -0,0 +1,12 @@
+// MonoIC compiler
+#ifndef IV_BREAKER_MONO_IC_H_
+#define IV_BREAKER_MONO_IC_H_
+namespace iv {
+namespace lv5 {
+namespace breaker {
+
+class MonoIC {
+};
+
+} } }  // namespace iv::lv5::breaker
+#endif  // IV_BREAKER_MONO_IC_H_
