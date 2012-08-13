cc88232a9e1c40fa18bdde87002aa19eef086dbe
Constellation <utatane.tea@gmail.com>
2012-08-13 23:27:12 +0900

    Updated platform.h

--- iv/platform.h
+++ iv/platform.h
@@ -5,8 +5,10 @@
   #include <sys/param.h>
 #endif
 
-#define IV_MAKE_VERSION(major, minor, patch)\
-    ((major) * 10000 + (minor) * 100 + (patch))
+#ifndef IV_MAKE_VERSION
+  #define IV_MAKE_VERSION(major, minor, patch)\
+      ((major) * 10000 + (minor) * 100 + (patch))
+#endif
 
 #if defined(_WIN32) || defined(_WIN64)
   #ifndef IV_OS_WIN
@@ -37,57 +39,80 @@
 
 // compiler
 #if defined(__GNUC__)
-  #if defined(__GNU_PATCHLEVEL__)
-    #define IV_COMPILER_GCC\
-      IV_MAKE_VERSION(__GNUC__, __GNUC_MINOR__, __GNU_PATCHLEVEL__)
-  #else
-    #define IV_COMPILER_GCC IV_MAKE_VERSION(__GNUC__, __GNUC_MINOR__, 0)
+  #ifndef IV_COMPILER_GCC
+    #if defined(__GNU_PATCHLEVEL__)
+      #define IV_COMPILER_GCC\
+        IV_MAKE_VERSION(__GNUC__, __GNUC_MINOR__, __GNU_PATCHLEVEL__)
+    #else
+      #define IV_COMPILER_GCC IV_MAKE_VERSION(__GNUC__, __GNUC_MINOR__, 0)
+    #endif
   #endif
 #elif defined(_MSC_VER)
-  #define IV_COMPILER_MSVC _MSC_VER
-  #define IV_COMPILER_MSVC_10 (_MSC_VER >= 1600)
+  #ifndef IV_COMPILER_MSVC
+    #define IV_COMPILER_MSVC _MSC_VER
+    #define IV_COMPILER_MSVC_10 (_MSC_VER >= 1600)
+  #endif
 #elif defined(__clang__)
-  #define IV_COMPILER_CLANG __clang__
+  #ifndef IV_COMPILER_CLANG
+    #if defined(__clang_patchlevel__)
+      #define IV_COMPILER_CLANG\
+        IV_MAKE_VERSION(__clang_major__, __clang_minor__, __clang_patchlevel__)
+    #else
+      #define IV_COMPILER_CLANG IV_MAKE_VERSION(__clang_major__, __clang_minor__, 0)
+    #endif
+  #endif
 #elif defined(__ICL) || defined(__ICC)
-  #if defined(__ICL)
-    #define IV_COMPILER_ICC __ICL
-  #else
-    #define IV_COMPILER_ICC __ICC
+  #ifndef IV_COMPILER_ICC
+    #if defined(__ICL)
+      #define IV_COMPILER_ICC __ICL
+    #else
+      #define IV_COMPILER_ICC __ICC
+    #endif
   #endif
 #endif
 
 // snprintf
 #if defined(IV_COMPILER_MSVC)
-  #define snprintf _snprintf_s
+  #ifndef snprintf
+    #define snprintf _snprintf_s
+  #endif
 #endif
 
 // 64bit system or not
-#if defined(__LP64__)
-  #define IV_64
-#elif defined(_WIN64)
-  #define IV_64
+#ifndef IV_64
+  #if defined(__LP64__)
+    #define IV_64
+  #elif defined(_WIN64)
+    #define IV_64
+  #endif
 #endif
 
 // CPU
 #if defined(__x86_64__) || defined(_WIN64)
-  #define IV_CPU_X64
+  #ifndef IV_CPU_X64
+    #define IV_CPU_X64
+  #endif
 #elif defined(__i386__)
-  #define IV_CPU_IA32
+  #ifndef IV_CPU_IA32
+    #define IV_CPU_IA32
+  #endif
 #elif defined(__arm__)
-  #define IV_CPU_ARM
+  #ifndef IV_CPU_ARM
+    #define IV_CPU_ARM
+  #endif
 #else
-  #define IV_CPU_OTHERS
+  #ifndef IV_CPU_OTHERS
+    #define IV_CPU_OTHERS
+  #endif
 #endif
 
 // SSE
-#if defined(__SSE1__) ||\
-    (defined(_M_IX86_FP) && _M_IX86_FP == 1)
+#if defined(__SSE1__) || (defined(_M_IX86_FP) && _M_IX86_FP == 1)
   #undef IV_CPU_SSE
   #define IV_CPU_SSE IV_MAKE_VERSION(1, 0, 0)
 #endif
 
-#if defined(__SSE2__) ||\
-    (defined(_M_IX86_FP) && _M_IX86_FP == 2)
+#if defined(__SSE2__) || (defined(_M_IX86_FP) && _M_IX86_FP == 2)
   #undef IV_CPU_SSE
   #define IV_CPU_SSE IV_MAKE_VERSION(2, 0, 0)
 #endif
@@ -95,14 +120,12 @@
 // Currenly, we cannot detect AVX archtecture on MSVC
 //
 // http://connect.microsoft.com/VisualStudio/feedback/details/605858/arch-avx-should-define-a-predefined-macro-in-x64-and-set-a-unique-value-for-m-ix86-fp-in-win32
-#if defined(__SSE3__) ||\
-    (defined(_M_IX86_FP) && _M_IX86_FP == 3)
+#if defined(__SSE3__) || (defined(_M_IX86_FP) && _M_IX86_FP == 3)
   #undef IV_CPU_SSE
   #define IV_CPU_SSE IV_MAKE_VERSION(3, 0, 0)
 #endif
 
-#if defined(__SSE4_1__) || \
-    (defined(_M_IX86_FP) && _M_IX86_FP == 3)
+#if defined(__SSE4_1__) || (defined(_M_IX86_FP) && _M_IX86_FP == 3)
   #undef IV_CPU_SSE
   #define IV_CPU_SSE IV_MAKE_VERSION(4, 1, 0)
 #endif
@@ -115,7 +138,7 @@
 // ENABLE_JIT
 // http://homepage1.nifty.com/herumi/diary/1109.html#7
 #if not +0
-  #if !defined(IV_DISABLE_JIT)
+  #ifndef IV_DISABLE_JIT
     #define IV_DISABLE_JIT
   #endif
 // #error "operator names used. use -fno-operator-names"
@@ -132,14 +155,14 @@
 #endif
 
 #if !defined(IV_ENABLE_JIT)
-  #if !defined(IV_DISABLE_JIT)
+  #ifndef IV_DISABLE_JIT
     #define IV_DISABLE_JIT
   #endif
 #endif
 
 #if defined(__GXX_EXPERIMENTAL_CXX0X__) ||\
     (defined(IV_COMPILER_MSVC) && defined(IV_COMPILER_MSVC_10))
-  #if !defined(IV_CXX11)
+  #ifndef IV_CXX11
     #define IV_CXX11
   #endif
 #endif
