c230d74bf23db2548751e18e1ba9d8201796439a
Constellation <utatane.tea@gmail.com>
2012-08-26 21:25:30 +0900

    Implement BitVector

---
+++ iv/bit_vector.h
@@ -0,0 +1,286 @@
+#ifndef IV_BIT_VECTOR_H_
+#define IV_BIT_VECTOR_H_
+#include <iv/detail/cstdint.h>
+#include <algorithm>
+namespace iv {
+namespace core {
+
+template<typename Alloc = std::allocator<uint64_t> >
+class BitVector {
+ public:
+  typedef Alloc allocator_type;
+  typedef std::size_t size_type;
+  typedef uint64_t value_type;
+  typedef value_type* pointer;
+  typedef const value_type* const_pointer;
+
+  typedef value_type word_type;
+  typedef BitVector<Alloc> this_type;
+
+  static const size_type kWordSize = 64;
+
+ private:
+  struct UnaryBitNot {
+    uint64_t operator()(uint64_t val) {
+      return ~val;
+    }
+  };
+
+  struct UnaryTrue {
+    bool operator()(uint64_t val) {
+      return val;
+    }
+  };
+
+ public:
+  explicit BitVector(const Alloc& alloc = Alloc())
+    : data_(NULL),
+      size_(0),
+      capacity_(0),
+      alloc_(alloc) {
+  }
+
+  explicit BitVector(size_type size,
+                     bool val = false, const Alloc& alloc = Alloc())
+    : data_(NULL),
+      size_(0),
+      capacity_(0),
+      alloc_(alloc) {
+    resize(size, val);
+  }
+
+  BitVector(const this_type& rhs)  // NOLINT
+    : data_(NULL),
+      size_(rhs.size()),
+      capacity_(Capacity(size())),
+      alloc_(rhs.get_allocator()) {
+    assert(capacity() <= rhs.capacity());
+    data_ = alloc_.allocate(capacity());
+    std::copy(rhs.data(), rhs.data() + capacity(), data());
+  }
+
+  this_type& operator=(const this_type& rhs) {
+    resize(rhs.size());
+    std::copy(rhs.data(), rhs.data() + num_words(), data());
+    return *this;
+  }
+
+  friend void swap(this_type& lhs, this_type& rhs) {
+    using std::swap;
+    swap(lhs.data_, rhs.data_);
+    swap(lhs.size_, rhs.size_);
+    swap(lhs.capacity_, rhs.capacity_);
+    swap(lhs.alloc_, rhs.alloc_);
+  }
+
+  void swap(this_type& rhs) {
+    using std::swap;
+    swap(*this, rhs);
+  }
+
+  ~BitVector() {
+    if (data()) {
+      alloc_.deallocate(data(), capacity());
+    }
+  }
+
+  bool operator[](size_type pos) const {
+    return data_[pos / kWordSize] &
+        (static_cast<value_type>(1) << (pos % kWordSize));
+  }
+
+  this_type& set() {
+    std::fill(data_, data_ + num_words(), UINT64_MAX);
+    return *this;
+  }
+
+  this_type& set(size_type pos, bool val = true) {
+    if (val) {
+      data_[pos / kWordSize] |=
+          (static_cast<value_type>(1) << (pos % kWordSize));
+    } else {
+      reset(pos);
+    }
+    return *this;
+  }
+
+  this_type& reset() {
+    std::fill(data_, data_ + num_words(), 0);
+    return *this;
+  }
+
+  this_type& reset(size_type pos) {
+    data_[pos / kWordSize] &=
+        ~(static_cast<value_type>(1) << (pos % kWordSize));
+    return *this;
+  }
+
+  void clear() {
+    size_ = 0;
+  }
+
+  this_type& flip() {
+    std::transform(data(), data() + num_words(), data(), UnaryBitNot());
+    return *this;
+  }
+
+  this_type& flip(size_type pos) {
+    if (test(pos)) {
+      return reset(pos);
+    } else {
+      return set(pos);
+    }
+  }
+
+  bool test(size_type pos) const {
+    return (*this)[pos];
+  }
+
+  void resize(size_type n, bool val = false) {
+    if (n <= size()) {
+      size_ = n;
+      return;
+    }
+
+    reserve(n);
+    const size_type old_num_words = num_words();
+    if (old_num_words) {
+      const size_type d = size() % kWordSize;
+      const word_type mask = UINT64_MAX << d;
+      if (val) {
+        data()[old_num_words - 1] |= mask;
+      } else {
+        data()[old_num_words - 1] &= ~mask;
+      }
+    }
+    const size_type d = Capacity(n) - old_num_words;
+    size_ = n;
+    if (d) {
+      const size_type full = d - 1;
+      if (full) {
+        if (val) {
+          std::fill(data() + old_num_words,
+                    data() + old_num_words + full, UINT64_MAX);
+        } else {
+          std::fill(data() + old_num_words,
+                    data() + old_num_words + full, 0);
+        }
+      }
+      // the last one
+      if (val) {
+        data()[old_num_words + full] = UINT64_MAX;
+      } else {
+        data()[old_num_words + full] = 0;
+      }
+    }
+  }
+
+  void reserve(size_type n) {
+    const size_type cap = Capacity(n);
+    if (cap > capacity()) {
+      pointer new_data = alloc_.allocate(capacity());
+      std::copy(data(), data() + Capacity(size()), new_data);
+      alloc_.deallocate(data(), capacity());
+      data_ = new_data;
+      capacity_ = cap;
+    }
+  }
+
+  this_type& operator&=(const this_type& rhs) {
+    assert(size() == rhs.size());
+    for (size_type i = 0, len = num_words(); i < len; ++i) {
+      data()[i] &= rhs.data()[i];
+    }
+  }
+
+  this_type& operator|=(const this_type& rhs) {
+    assert(size() == rhs.size());
+    for (size_type i = 0, len = num_words(); i < len; ++i) {
+      data()[i] |= rhs.data()[i];
+    }
+  }
+
+  this_type& operator^=(const this_type& rhs) {
+    assert(size() == rhs.size());
+    for (size_type i = 0, len = num_words(); i < len; ++i) {
+      data()[i] ^= rhs.data()[i];
+    }
+  }
+
+  this_type operator~() const {
+    return this_type(*this).flip();
+  }
+
+  friend bool operator==(const this_type& lhs, const this_type& rhs) {
+    if (lhs.size() != rhs.size()) {
+      return false;
+    }
+
+    const size_type n = lhs.num_words();
+    const size_type d = lhs.size() % kWordSize;
+
+    if (d == 0) {
+      return std::equal(lhs.data(), lhs.data() + n, rhs.data());
+    }
+
+    // when n == 0, d == 0
+    assert(n != 0);
+
+    if (n > 1) {
+      for (size_type i = 0, len = n - 1; i < len; ++i) {
+        if (lhs.data()[i] != rhs.data()[i]) {
+          return false;
+        }
+      }
+    }
+    // the last one
+    const word_type mask = UINT64_MAX >> (kWordSize - d);
+    return !((lhs.data()[n - 1] ^ rhs.data()[n - 1]) & mask);
+  }
+
+  friend bool operator!=(const this_type& lhs, const this_type& rhs) {
+    return !(lhs == rhs);
+  }
+
+  bool any() const {
+    const size_type n = num_words();
+    const size_type d = size() % kWordSize;
+    if (d == 0) {
+      return std::find_if(data(), data() + n, UnaryTrue()) != (data() + n);
+    }
+    if (n > 1) {
+      for (size_type i = 0, len = n - 1; i < len; ++i) {
+        if (data()[i]) {
+          return true;
+        }
+      }
+    }
+    // the last one
+    const word_type mask = UINT64_MAX >> (kWordSize - d);
+    return data()[n - 1] & mask;
+  }
+
+  bool none() const {
+    return !any();
+  }
+
+  pointer data() { return data_; }
+  const_pointer data() const { return data_; }
+  size_type size() const { return size_; }
+  bool empty() const { return size() == 0; }
+  size_type num_words() const { return Capacity(size()); }
+  size_type capacity() const { return capacity_; }
+  allocator_type get_allocator() const { return alloc_; }
+ private:
+  static size_type Capacity(size_type size) {
+    return (size == 0) ? 0 : 1 + ((size - 1) / kWordSize);
+  }
+
+  pointer data_;
+  size_type size_;
+  size_type capacity_;
+  allocator_type alloc_;
+};
+
+} }  // namespace iv::core
+#endif  // IV_BIT_VECTOR_H_
---
+++ test/test_bit_vector.cc
@@ -0,0 +1,56 @@
+#include <gtest/gtest.h>
+#include <iv/bit_vector.h>
+
+TEST(BitVectorCase, MainTest) {
+  iv::core::BitVector<> vec;
+  vec.resize(10, false);
+  EXPECT_TRUE(vec.none());
+  EXPECT_FALSE(vec.any());
+  EXPECT_EQ(10u, vec.size());
+  for (std::size_t i = 0; i < 10u; ++i) {
+    EXPECT_EQ(false, vec.test(i));
+  }
+  vec.set(0);
+  EXPECT_EQ(true, vec[0]);
+  vec.resize(20, false);
+  EXPECT_EQ(true, vec[0]);
+  EXPECT_TRUE(vec.any());
+  EXPECT_FALSE(vec.none());
+  for (std::size_t i = 1; i < 20u; ++i) {
+    EXPECT_EQ(false, vec.test(i));
+  }
+}
+
+TEST(BitVectorCase, FlipTest) {
+  iv::core::BitVector<> vec(10, false);
+  const iv::core::BitVector<> vec2 = ~vec;
+  for (std::size_t i = 0; i < 10u; ++i) {
+    EXPECT_EQ(false, vec.test(i));
+    EXPECT_EQ(true, vec2.test(i));
+  }
+  EXPECT_TRUE(vec != vec2);
+  EXPECT_FALSE(vec == vec2);
+  vec.flip();
+  for (std::size_t i = 0; i < 10u; ++i) {
+    EXPECT_EQ(true, vec.test(i));
+  }
+  EXPECT_FALSE(vec != vec2);
+  EXPECT_TRUE(vec == vec2);
+}
+
+TEST(BitVectorCase, EqualityTest) {
+  for (std::size_t i = 1; i < 129u; ++i) {
+    iv::core::BitVector<> vec(i, false);
+    iv::core::BitVector<> vec2 = vec;
+    EXPECT_TRUE(vec == vec2) << i;
+    EXPECT_FALSE(vec != vec2) << i;
+    vec.flip();
+    EXPECT_TRUE(vec != vec2) << i;
+    EXPECT_FALSE(vec == vec2) << i;
+  }
+
+  iv::core::BitVector<> vec1(0, false);
+  iv::core::BitVector<> vec2(0, false);
+  EXPECT_TRUE(vec1 == vec2);
+  EXPECT_TRUE(vec1.flip() == vec2);
+}
