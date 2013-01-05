0da4e771ab25383ad4e29f3cb133d504771bd1db
Constellation <utatane.tea@gmail.com>
2013-01-05 21:14:11 +0900

    Add BitCastNoGuard

--- iv/bit_cast.h
+++ iv/bit_cast.h
@@ -1,18 +1,24 @@
 #ifndef IV_BIT_CAST_H_
 #define IV_BIT_CAST_H_
+#include <iv/platform.h>
 #include <iv/static_assert.h>
 namespace iv {
 namespace core {
 
 template<typename To, typename From>
-inline To BitCast(From from) {
-  IV_STATIC_ASSERT(sizeof(To) == sizeof(From));
+IV_ALWAYS_INLINE To BitCastNoGuard(From from) {
   union {
     From from_;
     To to_;
   } value;
   value.from_ = from;
   return value.to_;
+}
+
+template<typename To, typename From>
+IV_ALWAYS_INLINE To BitCast(From from) {
+  IV_STATIC_ASSERT(sizeof(To) == sizeof(From));
+  return BitCastNoGuard<To, From>(from);
 }
 
 } }  // namespace iv::core
