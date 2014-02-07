ae07c6cde11c8cee2655cda8673c9ce17fb5b49b
Yusuke Suzuki <utatane.tea@gmail.com>
2014-02-07 19:42:13 +0900

    Use __sync_synchronize

--- iv/thread_safe_ref_counted.h
+++ iv/thread_safe_ref_counted.h
@@ -2,6 +2,7 @@
 #define IV_THREAD_SAFE_REF_COUNTED_H_
 #include <atomic>
 #include <iv/noncopyable.h>
+#include <iv/platform.h>
 namespace iv {
 namespace core {
 
@@ -24,7 +25,14 @@ class ThreadSafeRefCounted : private iv::core::Noncopyable<T> {
             std::memory_order_release) == 1) {
       // Memory fence is inserted to ensure delete should be executed after
       // decrementing the counter.
+#if defined(IV_COMPILER_GCC) && IV_COMPILER_GCC < IV_MAKE_VERSION(4, 7, 0)
+      // Memory fence operation is not implemented until GCC 4.7,
+      // so we use __sync_synchronize builtin function instead.
+      // http://stackoverflow.com/questions/16429669/stdatomic-thread-fence-has-undefined-reference
+      __sync_synchronize();
+#else
       std::atomic_thread_fence(std::memory_order_acquire);
+#endif
       delete static_cast<const T*>(this);
     }
   }
