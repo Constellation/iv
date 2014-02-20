949fcd677b241adc8090a81f604cc137e5979608
Yusuke Suzuki <utatane.tea@gmail.com>
2014-02-20 13:39:12 +0900

    Add diagram directory

---
+++ iv/lv5/diagram/compiler.cc
@@ -0,0 +1,6 @@
+// diagram::Compiler
+//
+// This compiler parses railgun::opcodes, inlines functions, and optimizes them
+// with LLVM.
+
+#include <llvm/LLVMContext.h>
---
+++ iv/lv5/diagram/compiler.h
@@ -0,0 +1,8 @@
+#ifndef IV_LV5_DIAGRAM_COMPILER_H_
+#define IV_LV5_DIAGRAM_COMPILER_H_
+namespace iv {
+namespace lv5 {
+namespace diagram {
+
+} } }  // namespace iv::lv5::diagram
+#endif  // IV_LV5_DIAGRAM_COMPILER_H_
