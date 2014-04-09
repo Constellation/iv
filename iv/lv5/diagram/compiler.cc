a4cd34c26bb6f099a6b0a4e022f13b516ff38947
iseki <iseki.m.aa@gmail.com>
2014-04-09 17:21:45 +0900

    Add entry points and set function pointer to railgun::Code

--- iv/lv5/diagram/compiler.cc
+++ iv/lv5/diagram/compiler.cc
@@ -14,16 +14,43 @@ class Compiler {
   typedef railgun::Instruction Instruction;
   typedef railgun::OP OP;
 
+  typedef std::unordered_map<railgun::Code*, std::string> EntryPointMap;
+  typedef std::vector<railgun::Code*> Codes;
+
   explicit Compiler(Context* ctx, railgun::Code* code, llvm::Module* module)
     : ctx_(ctx),
       code_(code),
+      codes_(),
       module_(module)
   { }
 
-  ~Compiler()
-  { }
+  ~Compiler() {
+
+    llvm::ExecutionEngine *EE = llvm::EngineBuilder(module_).create();
+    llvm::EngineBuilder(module_).create();
+    llvm::Function *F;
+    // link entry points
+    for (const auto& pair : entry_points_) {
+      F = module_->getFunction(llvm::StringRef(pair.second));
+      pair.first->set_executable(EE->getPointerToFunction(F));
+    }
+  }
+
+  static inline std::string MakeBlockName(railgun::Code* code) {
+    // example. Block 0x7fff5fbff8b812
+    char ptr[25];
+    sprintf(ptr,"Block %p", code);
+    return std::string(ptr);
+  }
+
+  void Initialize(railgun::Code* code) {
+    code_ = code;
+    codes_.push_back(code);
+    entry_points_.insert(std::make_pair(code, MakeBlockName(code)));
+  }
 
   void Compile(railgun::Code* code) {
+    Initialize(code);
     Main();
   }
 
@@ -555,6 +582,8 @@ class Compiler {
 
   Context* ctx_;
   railgun::Code* code_;
+  EntryPointMap entry_points_;
+  Codes codes_;
   llvm::Module *module_;
 };
 
