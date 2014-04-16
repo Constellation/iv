6b6ee34487359bd0286b5a4810ea85592da5a989
Yusuke Suzuki <utatane.tea@gmail.com>
2014-04-16 15:47:42 +0900

    Use static_assert to prevent error in gcc

--- iv/lv5/third_party/v8_dtoa/utils.h
+++ iv/lv5/third_party/v8_dtoa/utils.h
@@ -153,7 +153,9 @@ template <class Dest, class Source>
 inline Dest BitCast(const Source& source) {
   // Compile time assertion: sizeof(Dest) == sizeof(Source)
   // A compile error here means your Dest and Source have different sizes.
-  typedef char VerifySizesAreEqual[sizeof(Dest) == sizeof(Source) ? 1 : -1];
+  // typedef char VerifySizesAreEqual[sizeof(Dest) == sizeof(Source) ? 1 : -1];
+  static_assert(sizeof(Dest) == sizeof(Source),
+                "Dest and Source size should be equal");
 
   Dest dest;
   memcpy(&dest, &source, sizeof(dest));
